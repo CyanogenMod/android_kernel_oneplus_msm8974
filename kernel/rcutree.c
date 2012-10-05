b90f8890148d548faedcc2d4e1d56af1d5010086
Antti P Miettinen <ananaza@iki.fi>
2012-10-05 09:59:15 +0300

    rcu: Add a module parameter to force use of expedited RCU primitives
    There have been some embedded applications that would benefit from
    use of expedited grace-period primitives.  In some ways, this is
    similar to synchronize_net() doing either a normal or an expedited
    grace period depending on lock state, but with control outside of
    the kernel.
    
    This commit therefore adds rcu_expedited boot and sysfs parameters
    that cause the kernel to substitute expedited primitives for the
    normal grace-period primitives.
    
    [ paulmck: Add trace/event/rcu.h to kernel/srcu.c to avoid build error.
    	   Get rid of infinite loop through contention path.]
    
    CRs-fixed: 634363
    Change-Id: I45addcb532fdaa47df3019ada3283e293ed40249
    Signed-off-by: Antti P Miettinen <amiettinen@nvidia.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Git-commit: 3705b88db0d7cc4a097c32d9e554054103d3f807
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    [ joonwoop@codeaurora.org: resolved merge conflicts.]
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -1924,7 +1924,10 @@ void synchronize_sched(void)
 			   "Illegal synchronize_sched() in RCU-sched read-side critical section");
 	if (rcu_blocking_is_gp())
 		return;
-	wait_rcu_gp(call_rcu_sched);
+	if (rcu_expedited)
+		synchronize_sched_expedited();
+	else
+		wait_rcu_gp(call_rcu_sched);
 }
 EXPORT_SYMBOL_GPL(synchronize_sched);
 
@@ -1945,7 +1948,10 @@ void synchronize_rcu_bh(void)
 			   "Illegal synchronize_rcu_bh() in RCU-bh read-side critical section");
 	if (rcu_blocking_is_gp())
 		return;
-	wait_rcu_gp(call_rcu_bh);
+	if (rcu_expedited)
+		synchronize_rcu_bh_expedited();
+	else
+		wait_rcu_gp(call_rcu_bh);
 }
 EXPORT_SYMBOL_GPL(synchronize_rcu_bh);
 
