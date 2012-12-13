902667938f9a716e425f84b849d59abff7318d0c
Misael Lopez Cruz <misael.lopez@ti.com>
2012-12-13 12:23:05 -0600

    ASoC: Prevent pop_wait overwrite
    pop_wait is used to determine if a deferred playback close
    needs to be cancelled when the a PCM is open or if after
    the power-down delay expires it needs to run. pop_wait is
    associated with the CODEC DAI, so the CODEC DAI must be
    unique. This holds true for most CODECs, except for the
    dummy CODEC and its DAI.
    
    In DAI links with non-unique dummy CODECs (e.g. front-ends),
    pop_wait can be overwritten by another DAI link using also a
    dummy CODEC. Failure to cancel a deferred close can cause
    mute due to the DAPM STOP event sent in the deferred work.
    
    One scenario where pop_wait is overwritten and causing mute
    is below (where hw:0,0 and hw:0,1 are two front-ends with
    default pmdown_time = 5 secs):
    
    aplay /dev/urandom -D hw:0,0 -c 2 -r 48000 -f S16_LE -d 1
    sleep 1
    aplay /dev/urandom -D hw:0,1 -c 2 -r 48000 -f S16_LE -d 3 &
    aplay /dev/urandom -D hw:0,0 -c 2 -r 48000 -f S16_LE
    
    Since CODECs may not be unique, pop_wait is moved to the PCM
    runtime structure. Creating separate dummy CODECs for each
    DAI link can also solve the problem, but at this point it's
    only pop_wait variable in the CODEC DAI that has negative
    effects by not being unique.
    
    Change-Id: Ied2c60c8a643d971e0f865e04d75daeed1cb8ed4
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- include/sound/soc.h
+++ include/sound/soc.h
@@ -1029,6 +1029,7 @@ struct snd_soc_pcm_runtime {
 	int fe_compr;
 
 	long pmdown_time;
+	unsigned char pop_wait:1;
 
 	/* err in case of ops failed */
 	int err_ops;
--- sound/soc/soc-compress.c
+++ sound/soc/soc-compress.c
@@ -186,11 +186,11 @@ static void close_delayed_work(struct work_struct *work)
 	dev_dbg(rtd->dev, "ASoC: pop wq checking: %s status: %s waiting: %s\n",
 		 codec_dai->driver->playback.stream_name,
 		 codec_dai->playback_active ? "active" : "inactive",
-		     codec_dai->pop_wait ? "yes" : "no");
+		     rtd->pop_wait ? "yes" : "no");
 
 	/* are we waiting on this codec DAI stream */
-	if (codec_dai->pop_wait == 1) {
-	  codec_dai->pop_wait = 0;
+	if (rtd->pop_wait == 1) {
+	  rtd->pop_wait = 0;
 		snd_soc_dapm_stream_event(rtd, SNDRV_PCM_STREAM_PLAYBACK,
 					  SND_SOC_DAPM_STREAM_STOP);
 	}
@@ -242,7 +242,7 @@ static int soc_compr_free(struct snd_compr_stream *cstream)
 					codec_dai->driver->playback.stream_name,
 					SND_SOC_DAPM_STREAM_STOP);
 		} else {
-			codec_dai->pop_wait = 1;
+			rtd->pop_wait = 1;
 			schedule_delayed_work(&rtd->delayed_work,
 				msecs_to_jiffies(rtd->pmdown_time));
 		}
@@ -457,7 +457,7 @@ static int soc_compr_set_params(struct snd_compr_stream *cstream,
 				SND_SOC_DAPM_STREAM_START);
 
 	/* cancel any delayed stream shutdown that is pending */
-	codec_dai->pop_wait = 0;
+	rtd->pop_wait = 0;
 	mutex_unlock(&rtd->pcm_mutex);
 
 	cancel_delayed_work_sync(&rtd->delayed_work);
