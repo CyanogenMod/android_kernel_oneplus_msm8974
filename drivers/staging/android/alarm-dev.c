3a59b409feefd8fea4bf1bf01341d1fc3d8c70b3
Dan Carpenter <dan.carpenter@oracle.com>
2013-06-03 02:02:31 -0700

    staging: alarm-dev: information leak in alarm_ioctl()
    Smatch complains that if we pass an invalid clock type then "ts" is
    never set.  We need to check for errors earlier, otherwise we end up
    passing uninitialized stack data to userspace.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Acked-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/alarm-dev.c
+++ drivers/staging/android/alarm-dev.c
@@ -286,6 +286,8 @@ static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	}
 
 	rv = alarm_do_ioctl(file, cmd, &ts);
+	if (rv)
+		return rv;
 
 	switch (ANDROID_ALARM_BASE_CMD(cmd)) {
 		case ANDROID_ALARM_GET_TIME(0):
@@ -294,7 +296,7 @@ static long alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 			break;
 	}
 
-	return rv;
+	return 0;
 }
 
 static int alarm_open(struct inode *inode, struct file *file)
