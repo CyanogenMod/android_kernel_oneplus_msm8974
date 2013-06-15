ad1dcef8c13b6d2af54e4655e5a125e8d4d854de
Francisco Franco <franciscofranco.1990@gmail.com>
2013-06-15 20:57:59 +0100

    writeback: increase bdi_min_ratio to 5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -458,7 +458,7 @@ static void bdi_writeout_fraction(struct backing_dev_info *bdi,
  * registered backing devices, which, for obvious reasons, can not
  * exceed 100%.
  */
-static unsigned int bdi_min_ratio;
+static unsigned int bdi_min_ratio = 5;
 
 int bdi_set_min_ratio(struct backing_dev_info *bdi, unsigned int min_ratio)
 {
