de9032ce352decff7fc0893e1878d353841d4dc4
David Collins <collinsd@codeaurora.org>
2013-07-08 14:33:22 -0700

    regulator: Call proxy-consumer functions for each regulator registered
    Add a call to regulator_proxy_consumer_register() at the end of
    regulator_register() and a call to
    regulator_proxy_consumer_unregister() at the beginning of
    regulator_unregister().  These calls ensure that proxy consumer
    features may be used by any type of regulator regardless of the
    driver supporting it.
    
    Change-Id: Ia70da1df47042fa673c42f389136557f868d19a3
    Signed-off-by: David Collins <collinsd@codeaurora.org>

--- include/linux/regulator/driver.h
+++ include/linux/regulator/driver.h
@@ -18,6 +18,7 @@
 #include <linux/device.h>
 #include <linux/notifier.h>
 #include <linux/regulator/consumer.h>
+#include <linux/regulator/proxy-consumer.h>
 
 struct regulator_dev;
 struct regulator_init_data;
@@ -208,6 +209,8 @@ struct regulator_dev {
 	void *reg_data;		/* regulator_dev data */
 
 	struct dentry *debugfs;
+
+	struct proxy_consumer *proxy_consumer;
 };
 
 struct regulator_dev *regulator_register(struct regulator_desc *regulator_desc,
