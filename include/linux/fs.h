935dd62879daabf37a068003f85dae9546dcba4d
Jan Kara <jack@suse.cz>
2013-07-09 22:36:45 +0800

    writeback: Do not sort b_io list only because of block device inode
    It is very likely that block device inode will be part of BDI dirty list
    as well. However it doesn't make sence to sort inodes on the b_io list
    just because of this inode (as it contains buffers all over the device
    anyway). So save some CPU cycles which is valuable since we hold relatively
    contented wb->list_lock.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Change-Id: Ifc9e108215fcbaf967dbc7f0a629b38e1d178e47

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2065,6 +2065,7 @@ extern struct super_block *freeze_bdev(struct block_device *);
 extern void emergency_thaw_all(void);
 extern int thaw_bdev(struct block_device *bdev, struct super_block *sb);
 extern int fsync_bdev(struct block_device *);
+extern int sb_is_blkdev_sb(struct super_block *sb);
 #else
 static inline void bd_forget(struct inode *inode) {}
 static inline int sync_blockdev(struct block_device *bdev) { return 0; }
@@ -2077,6 +2078,11 @@ static inline struct super_block *freeze_bdev(struct block_device *sb)
 }
 
 static inline int thaw_bdev(struct block_device *bdev, struct super_block *sb)
+{
+	return 0;
+}
+
+static inline int sb_is_blkdev_sb(struct super_block *sb)
 {
 	return 0;
 }
