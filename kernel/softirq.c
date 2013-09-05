6ab843c7ee73cdcb231d61bdfa085a08677a839a
Frederic Weisbecker <fweisbec@gmail.com>
2013-09-05 16:14:00 +0200

    irq: Justify the various softirq stack choices
    For clarity, comment the various stack choices for softirqs
    processing, whether we execute them from ksoftirqd or
    local_irq_enable() calls.
    
    Their use on irq_exit() is already commented.
    
    Acked-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@au1.ibm.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Paul Mackerras <paulus@au1.ibm.com>
    Cc: James Hogan <james.hogan@imgtec.com>
    Cc: James E.J. Bottomley <jejb@parisc-linux.org>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Cc: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	kernel/softirq.c

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -171,8 +171,13 @@ static inline void _local_bh_enable_ip(unsigned long ip)
  	 */
 	sub_preempt_count(SOFTIRQ_DISABLE_OFFSET - 1);
 
-	if (unlikely(!in_interrupt() && local_softirq_pending()))
+	if (unlikely(!in_interrupt() && local_softirq_pending())) {
+		/*
+		 * Run softirq if any pending. And do it in its own stack
+		 * as we may be calling this deep in a task call stack already.
+		 */
 		do_softirq();
+	}
 
 	dec_preempt_count();
 #ifdef CONFIG_TRACE_IRQFLAGS
@@ -277,8 +282,6 @@ restart:
 	WARN_ON_ONCE(in_interrupt());
 }
 
-
-
 asmlinkage void do_softirq(void)
 {
 	__u32 pending;
@@ -345,7 +348,6 @@ void irq_exit(void)
 #else
 	WARN_ON_ONCE(!irqs_disabled());
 #endif
-
 	account_system_vtime(current);
 	trace_hardirq_exit();
 	sub_preempt_count(HARDIRQ_OFFSET);
@@ -760,8 +762,27 @@ static void run_ksoftirqd(unsigned int cpu)
 		local_irq_enable();
 		cond_resched();
 
-		preempt_disable();
-		rcu_note_context_switch(cpu);
+		__set_current_state(TASK_RUNNING);
+
+		while (local_softirq_pending()) {
+			/* Preempt disable stops cpu going offline.
+			   If already offline, we'll be on wrong CPU:
+			   don't process */
+			if (cpu_is_offline((long)__bind_cpu))
+				goto wait_to_die;
+			local_irq_disable();
+			if (local_softirq_pending())
+		/*
+		 * We can safely run softirq on inline stack, as we are not deep
+		 * in the task stack here.
+		 */
+				__do_softirq();
+			local_irq_enable();
+			sched_preempt_enable_no_resched();
+			cond_resched();
+			preempt_disable();
+			rcu_note_context_switch((long)__bind_cpu);
+		}
 		preempt_enable();
 
 		return;
