ab37e1864b54315e00afe965e84a1f04b35a0049
Eakkasit L <ramgear@gmail.com>
2013-09-16 17:39:52 +0200

    Readahead: Optimize divide/multiply by power of 2 using L/R shift
    Signed-off-by: Chet <Cl3Kener@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- mm/readahead.c
+++ mm/readahead.c
@@ -221,7 +221,7 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 	while (nr_to_read) {
 		int err;
 
-		unsigned long this_chunk = (2 * 1024 * 1024) / PAGE_CACHE_SIZE;
+		unsigned long this_chunk = (2097152) >> PAGE_CACHE_SHIFT;
 
 		if (this_chunk > nr_to_read)
 			this_chunk = nr_to_read;
@@ -245,7 +245,7 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 unsigned long max_sane_readahead(unsigned long nr)
 {
 	return min(nr, (node_page_state(numa_node_id(), NR_INACTIVE_FILE)
-		+ node_page_state(numa_node_id(), NR_FREE_PAGES)) / 2);
+		+ node_page_state(numa_node_id(), NR_FREE_PAGES)) >> 1);
 }
 
 /*
@@ -274,10 +274,10 @@ static unsigned long get_init_ra_size(unsigned long size, unsigned long max)
 {
 	unsigned long newsize = roundup_pow_of_two(size);
 
-	if (newsize <= 1)
-		newsize = newsize * 4;
-	else if (newsize <= max / 4)
-		newsize = newsize * 2;
+	if (newsize <= (max >> 5))
+		newsize = newsize << 2;
+	else if (newsize <= (max >> 2))
+		newsize = newsize << 1;
 	else
 		newsize = max;
 
@@ -294,10 +294,10 @@ static unsigned long get_next_ra_size(struct file_ra_state *ra,
 	unsigned long cur = ra->size;
 	unsigned long newsize;
 
-	if (cur < max / 16)
-		newsize = 4 * cur;
+	if (cur < (max >> 4))
+		newsize = cur << 2;
 	else
-		newsize = 2 * cur;
+		newsize = cur << 1;
 
 	return min(newsize, max);
 }
@@ -385,7 +385,7 @@ static int try_context_readahead(struct address_space *mapping,
 	 * it is a strong indication of long-run stream (or whole-file-read)
 	 */
 	if (size >= offset)
-		size *= 2;
+		size <<= 1;
 
 	ra->start = offset;
 	ra->size = min(size + req_size, max);
