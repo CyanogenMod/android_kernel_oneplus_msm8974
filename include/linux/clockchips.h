e15d5f12d602d44d1db9aa3026adf47a3ecc3725
Soren Brinkmann <soren.brinkmann@xilinx.com>
2013-09-18 11:48:35 -0700

    clockchips: Add FEAT_PERCPU clockevent flag
    Add the flag CLOCK_EVT_FEAT_PERCPU which is supposed to be set for per
    cpu clockevent devices.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Signed-off-by: Daniel Lezcano <daniel.lezcano@linaro.org>
    Acked-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/clockchips.h
+++ include/linux/clockchips.h
@@ -59,6 +59,7 @@ enum clock_event_nofitiers {
  * Core shall set the interrupt affinity dynamically in broadcast mode
  */
 #define CLOCK_EVT_FEAT_DYNIRQ		0x000020
+#define CLOCK_EVT_FEAT_PERCPU		0x000040
 
 /**
  * struct clock_event_device - clock event device descriptor
