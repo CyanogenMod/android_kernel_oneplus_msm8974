9489d0e5fb9327bbc608c5b6e1a21c698bbc9ed3
Srinivasarao P <spathi@codeaurora.org>
2013-09-18 14:33:45 +0530

    workqueue: Added null check and warning
    If work item is already in queue get_work_cwq() will return null.
    so null check is added to avoid crash in delayed_work_timer_on()
    
    CRs-Fixed: 586162
    
    Change-Id: I5c885142fd72781cea90d6c9b3456ecc7f0778b6
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -572,7 +572,10 @@ static struct cpu_workqueue_struct *get_work_cwq(struct work_struct *work)
 	if (data & WORK_STRUCT_CWQ)
 		return (void *)(data & WORK_STRUCT_WQ_DATA_MASK);
 	else
+	{
+		WARN_ON_ONCE(1);
 		return NULL;
+	}
 }
 
 static struct global_cwq *get_work_gcwq(struct work_struct *work)
@@ -1093,7 +1096,8 @@ static void delayed_work_timer_fn(unsigned long __data)
 	struct delayed_work *dwork = (struct delayed_work *)__data;
 	struct cpu_workqueue_struct *cwq = get_work_cwq(&dwork->work);
 
-	__queue_work(smp_processor_id(), cwq->wq, &dwork->work);
+	if (cwq != NULL)
+		__queue_work(smp_processor_id(), cwq->wq, &dwork->work);
 }
 
 /**
