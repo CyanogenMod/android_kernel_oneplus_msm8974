6ddfe4261d1799aa46abdae916a109c14dfdf621
Laura Abbott <lauraa@codeaurora.org>
2013-11-14 11:29:38 -0800

    cma: Change to reserve-contiguous-region
    Currently, devices are associated with CMA regions via phandle to the
    CMA region. The phandle name 'linux,contiguous-region' is the same as
    that which is used to designate a CMA region. This can lead to issues
    of trying to treat a device only using a CMA region as an actual CMA
    region. Rather than continuing to rely on this handle and the depth
    to differentiate CMA regions, just create separate DT binding
    linux,reserve-contiguous-region to indicate that the node is
    an actual CMA node for reserving rather than a client referencing
    via phandle.
    
    Change-Id: I88b2d86054525b0569efc424da87974509ce9b25
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -231,7 +231,7 @@ int __init cma_fdt_scan(unsigned long node, const char *uname,
 	phys_addr_t limit = MEMBLOCK_ALLOC_ANYWHERE;
 	char *status;
 
-	if (!of_get_flat_dt_prop(node, "linux,contiguous-region", NULL))
+	if (!of_get_flat_dt_prop(node, "linux,reserve-contiguous-region", NULL))
 		return 0;
 
 	status = of_get_flat_dt_prop(node, "status", NULL);
