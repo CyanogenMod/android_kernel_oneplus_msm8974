a772f720f400b14b169ebf6787d5670273ee0e52
Tk-Glitch <Ti3noU@gmail.com>
2013-12-21 10:21:12 +0100

    sched: Add sysinterface for GENTLE_FAIR_SLEEPERS
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/ksysfs.c
+++ kernel/ksysfs.c
@@ -207,9 +207,46 @@ static struct attribute_group kernel_attr_group = {
 	.attrs = kernel_attrs,
 };
 
+
+static unsigned int Lgentle_fair_sleepers = 1;
+extern void relay_gfs(unsigned int gfs);
+
+static ssize_t gentle_fair_sleepers_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u\n", Lgentle_fair_sleepers);
+}
+static ssize_t gentle_fair_sleepers_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret, cpu;
+	ret = sscanf(buf, "%u", &input);
+	if (input != 0 && input != 1)
+		input = 0;
+	
+	Lgentle_fair_sleepers = input;
+	relay_gfs(Lgentle_fair_sleepers);
+	return count;
+}
+
+static struct kobj_attribute gentle_fair_sleepers_attribute =
+__ATTR(gentle_fair_sleepers, 0666, gentle_fair_sleepers_show, gentle_fair_sleepers_store);
+
+static struct attribute *gentle_fair_sleepers_attrs[] = {
+&gentle_fair_sleepers_attribute.attr,
+NULL,
+};
+
+static struct attribute_group gentle_fair_sleepers_attr_group = {
+.attrs = gentle_fair_sleepers_attrs,
+};
+
+/* Initialize fast charge sysfs folder */
+static struct kobject *gentle_fair_sleepers_kobj;
+
 static int __init ksysfs_init(void)
 {
 	int error;
+	int retval;
 
 	kernel_kobj = kobject_create_and_add("kernel", NULL);
 	if (!kernel_kobj) {
@@ -219,6 +256,12 @@ static int __init ksysfs_init(void)
 	error = sysfs_create_group(kernel_kobj, &kernel_attr_group);
 	if (error)
 		goto kset_exit;
+
+	gentle_fair_sleepers_kobj = kobject_create_and_add("sched", kernel_kobj);
+	retval = sysfs_create_group(gentle_fair_sleepers_kobj, &gentle_fair_sleepers_attr_group);
+
+	if (retval)
+		kobject_put(gentle_fair_sleepers_kobj);
 
 	if (notes_size > 0) {
 		notes_attr.size = notes_size;
--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1054,6 +1054,12 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
+static unsigned int Lgentle_fair_sleepers = 1;
+void relay_gfs(unsigned int gfs)
+{
+	Lgentle_fair_sleepers = gfs;
+}
+
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -1076,7 +1082,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
-		if (sched_feat(GENTLE_FAIR_SLEEPERS))
+		if (Lgentle_fair_sleepers)
 			thresh >>= 1;
 
 		vruntime -= thresh;
