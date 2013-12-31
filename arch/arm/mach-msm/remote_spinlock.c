8ec68595f3d6f00b28bd3f852054c6fe8ba9a652
Pranav Vashi <neobuddy89@gmail.com>
2013-12-31 08:03:40 -0800

    msm: use of swp{b} is deprecated for ARMv6+
    Change-Id: I6ac5b459dcdc0b4a8d88df12243dfeb9468bcc0d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/remote_spinlock.c
+++ arch/arm/mach-msm/remote_spinlock.c
@@ -70,6 +70,10 @@
 #define CURRENT_MODE_INIT AUTO_MODE;
 #endif
 
+#if defined(CONFIG_THUMB2_KERNEL) || defined(CONFIG_ARCH_MSM8974)
+#define SWP_OFF
+#endif
+
 static int current_mode = CURRENT_MODE_INIT;
 
 static int is_hw_lock_type;
@@ -146,7 +150,7 @@ static int __raw_remote_dek_spin_owner(raw_remote_spinlock_t *lock)
 }
 /* end dekkers implementation ----------------------------------------------- */
 
-#ifndef CONFIG_THUMB2_KERNEL
+#ifndef SWP_OFF
 /* swp implementation ------------------------------------------------------- */
 static void __raw_remote_swp_spin_lock(raw_remote_spinlock_t *lock)
 {
@@ -457,7 +461,7 @@ static void initialize_ops(void)
 		current_ops.owner = __raw_remote_dek_spin_owner;
 		is_hw_lock_type = 0;
 		break;
-#ifndef CONFIG_THUMB2_KERNEL
+#ifndef SWP_OFF
 	case SWP_MODE:
 		current_ops.lock = __raw_remote_swp_spin_lock;
 		current_ops.unlock = __raw_remote_swp_spin_unlock;
