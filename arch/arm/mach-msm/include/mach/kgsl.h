8a5ce8c54244ad79f683a19ed6dc4fa15fdb635d
Prakash Kamliya <pkamliya@codeaurora.org>
2014-01-21 00:29:34 +0530

    msm: kgsl: get pm_qos_latency from device tree
    pm_qos_latency parameter can be configured from
    device tree as well. Will set to default value if
    entry not found in target device tree.
    
    Conflicts:
    	include/linux/msm_kgsl.h
    
    Change-Id: I76271f4fdc26e191431e5dfe2a6de9e2b3ab426f
    Signed-off-by: Prakash Kamliya <pkamliya@codeaurora.org>

--- arch/arm/mach-msm/include/mach/kgsl.h
+++ arch/arm/mach-msm/include/mach/kgsl.h
@@ -86,6 +86,7 @@ struct kgsl_device_platform_data {
 	struct coresight_device *csdev;
 	struct coresight_platform_data *coresight_pdata;
 	unsigned int chipid;
+	unsigned int pm_qos_latency;
 };
 
 #endif
