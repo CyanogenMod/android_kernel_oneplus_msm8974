52911471bbc9d6624dc0ab49ce5073731dcea47e
Ethan Chen <intervigil@gmail.com>
2014-02-03 17:40:59 -0800

    msm: mdss: set lm3630 backlight level
    Change-Id: I59ed371c27982448eeeec10ad7f2509f3fca7cac

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -28,6 +28,10 @@
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
 
+#ifdef CONFIG_MACH_OPPO
+extern int lm3630_bank_a_update_status(u32 bl_level);
+#endif
+
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	ctrl->pwm_bl = pwm_request(ctrl->pwm_lpg_chan, "lcd-bklt");
@@ -303,6 +307,9 @@ static void mdss_dsi_panel_bl_ctrl(struct mdss_panel_data *pdata,
 			__func__);
 		break;
 	}
+#ifdef CONFIG_MACH_OPPO
+	lm3630_bank_a_update_status(bl_level);
+#endif
 }
 
 static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
