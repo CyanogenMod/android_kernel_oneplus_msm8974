25d993bf739a5c79c24e0ea0d53729366ef90cd3
Danesh M <daneshm90@gmail.com>
2014-02-10 15:42:37 -0800

    input: touchscreen: synaptics_s3508: Add custom scancodes for gestures
    Change-Id: Ia57627c31088e29e3eda3daaaf965fee8f6ccc48

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -1847,7 +1847,7 @@ static unsigned char synaptics_rmi4_update_gesture2(unsigned char *gesture,unsig
 	switch(gesture[0]) {
 	case SYNA_ONE_FINGER_CIRCLE:
 		gesturemode = Circle ;
-        keyvalue = KEY_F4;
+        keyvalue = KEY_GESTURE_CIRCLE;
 		break ;
 		
 	case SYNA_TWO_FINGER_SWIPE:
@@ -1867,33 +1867,33 @@ static unsigned char synaptics_rmi4_update_gesture2(unsigned char *gesture,unsig
 		}
 
 		if(gesturemode!=UnkownGestrue)	{  
-            keyvalue = KEY_F6;
+            keyvalue = KEY_GESTURE_SWIPE_DOWN;
 		} 
 		
 		break ;
 		
 	case SYNA_ONE_FINGER_DOUBLE_TAP:
 		gesturemode = DouTap ;
-        keyvalue = KEY_F3;
+        keyvalue = KEY_POWER;
 		break ;
 		
 	case SYNA_ONE_FINGER_DIRECTION:
 		switch(gesture[2]){
 		case 0x01:  //UP
 			gesturemode = DownVee ;
-			keyvalue = KEY_F5;
+			keyvalue = KEY_GESTURE_V;
 			break;
 		case 0x02:  //DOWN
 			gesturemode = UpVee ;
-			keyvalue = KEY_F5;
+			keyvalue = KEY_GESTURE_V;
 			break;
 		case 0x04:  //LEFT
 			gesturemode = RightVee ;
-			keyvalue = KEY_F7;
+			keyvalue = KEY_GESTURE_LTR;
 			break;
 		case 0x08:  //RIGHT
 			gesturemode = LeftVee ;
-			keyvalue = KEY_F8;
+			keyvalue = KEY_GESTURE_GTR;
 			break;
 		}
 		break;
@@ -2006,8 +2006,6 @@ static int synaptics_rmi4_f12_abs_report(struct synaptics_rmi4_data *rmi4_data,
 				sizeof(gestureext));
 	    if(gesture[0]) {
 				keyvalue = synaptics_rmi4_update_gesture2(gesture,gestureext);
-				if(syna_use_gesture2 && keyvalue)
-					keyvalue = KEY_F4 ;
 				if(keyvalue != KEY_F9 && keyvalue) {
 				    input_report_key(rmi4_data->input_dev, keyvalue, 1);
 					input_sync(rmi4_data->input_dev);
@@ -3315,12 +3313,12 @@ static void synaptics_rmi4_set_params(struct synaptics_rmi4_data *rmi4_data)
 	set_bit(KEY_MENU, rmi4_data->input_dev->keybit);
 	set_bit(KEY_HOMEPAGE, rmi4_data->input_dev->keybit);
 	set_bit(KEY_F3, rmi4_data->input_dev->keybit);
-    set_bit(KEY_F4, rmi4_data->input_dev->keybit);
-    set_bit(KEY_F5, rmi4_data->input_dev->keybit);
-    set_bit(KEY_F6, rmi4_data->input_dev->keybit);
-    set_bit(KEY_F7, rmi4_data->input_dev->keybit);
-    set_bit(KEY_F8, rmi4_data->input_dev->keybit);
-    set_bit(KEY_POWER, rmi4_data->input_dev->keybit);
+	set_bit(KEY_POWER, rmi4_data->input_dev->keybit);
+	set_bit(KEY_GESTURE_CIRCLE, rmi4_data->input_dev->keybit);
+	set_bit(KEY_GESTURE_SWIPE_DOWN, rmi4_data->input_dev->keybit);
+	set_bit(KEY_GESTURE_V, rmi4_data->input_dev->keybit);
+	set_bit(KEY_GESTURE_LTR, rmi4_data->input_dev->keybit);
+	set_bit(KEY_GESTURE_GTR, rmi4_data->input_dev->keybit);
     synaptics_ts_init_virtual_key(rmi4_data) ;
 
 	input_set_abs_params(rmi4_data->input_dev,
