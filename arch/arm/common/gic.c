e3e55ad44631dd4f5891139d6ddf328a651fe247
Ruchi Kandoi <kandoiruchi@google.com>
2014-02-24 11:04:45 -0800

    POWER: Add an API call to log wake wakeup reason
    Change-Id: I8bf670b07dcd52e29214ebb4e6e42dddd8c3d35e
    Signed-off-by: Ruchi kandoi <kandoiruchi@google.com>

--- arch/arm/common/gic.c
+++ arch/arm/common/gic.c
@@ -40,6 +40,7 @@
 #include <linux/percpu.h>
 #include <linux/slab.h>
 #include <linux/syscore_ops.h>
+#include <linux/wakeup_reason.h>
 
 #include <asm/irq.h>
 #include <asm/exception.h>
@@ -267,7 +268,6 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 	for (i = find_first_bit(pending, gic->gic_irqs);
 		i < gic->gic_irqs;
 		i = find_next_bit(pending, gic->gic_irqs, i+1)) {
-		log_base_wakeup_reason(i + gic->irq_offset);
 		struct irq_desc *desc = irq_to_desc(i + gic->irq_offset);
 		const char *name = "null";
 
@@ -278,6 +278,8 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 
 		pr_warning("%s: %d triggered %s\n", __func__,
 					i + gic->irq_offset, name);
+
+		log_base_wakeup_reason(i + gic->irq_offset);
 	}
 }
 
