fe1f6716f671c31fb72dd4d6c5b2cececbb8c6fe
Chester Kener <Cl3Kener@gmail.com>
2014-02-25 10:25:31 -0500

    block: Disable IO Stats
    * I see no need to have this enabled.  Will test to verify that it is not needed.
    
    Change-Id: I1d79d6eea8e2ff60b3d6b4f96d187a1d1863fa98
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -432,7 +432,7 @@ struct request_queue {
 #define QUEUE_FLAG_SAME_FORCE  18	/* force complete on same CPU */
 #define QUEUE_FLAG_SANITIZE    19	/* supports SANITIZE */
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
+#define QUEUE_FLAG_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_ADD_RANDOM))
