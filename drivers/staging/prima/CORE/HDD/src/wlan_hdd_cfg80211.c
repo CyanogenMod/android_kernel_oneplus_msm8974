af9b92613a6e91becf815711285f8825ec07ba2f
Steve Kondik <shade@chemlab.org>
2014-04-08 18:51:55 -0700

    prima: Fix ifdefs for building without TDLS
    Change-Id: I24ac3d82de6c919b392087e68eb9e471885a0143

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg80211.c
@@ -7179,6 +7179,7 @@ static int wlan_hdd_cfg80211_add_station(struct wiphy *wiphy,
 {
     int status = -EPERM;
 #ifdef FEATURE_WLAN_TDLS
+    hdd_adapter_t *pAdapter =  WLAN_HDD_GET_PRIV_PTR(dev);
     u32 mask, set;
     ENTER();
     mask = params->sta_flags_mask;
