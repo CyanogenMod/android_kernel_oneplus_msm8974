f3a55feefd5a1dadf5837cf42376d4a321ffa3b1
Susheel Khiani <skhiani@codeaurora.org>
2014-04-26 14:03:55 +0530

    Revert "lowmemorykiller: Don't count reserve page twice"
    This reverts commit d73f05ceab2717c23f00bfeffa31a7dedd15569a.
    
    The patch is causing device crash while running
    kernel tests on mainline. The change was to
    make LMK less aggressive.
    
    Change-Id: I272bf5ccd54a29de1c770d69af7fea2963f46333
    Signed-off-by: Susheel Khiani <skhiani@codeaurora.org>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -35,6 +35,7 @@
 #include <linux/mm.h>
 #include <linux/oom.h>
 #include <linux/sched.h>
+#include <linux/swap.h>
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
 #include <linux/mutex.h>
@@ -277,7 +278,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 			return 0;
 	}
 
-	other_free = global_page_state(NR_FREE_PAGES);
+	other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 
 	if (global_page_state(NR_SHMEM) + total_swapcache_pages <
 		global_page_state(NR_FILE_PAGES))
