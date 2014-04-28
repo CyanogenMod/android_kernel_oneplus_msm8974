cee609c3c79b5deae5201d94907c3260a767b03b
Steve Kondik <shade@chemlab.org>
2014-04-28 09:01:51 +0800

    sound: usb: Use digital audio type for userspace notification
     * Devices connected via USB should appear as "digital" devices
       which do not use Android's volume controls.
    
    Change-Id: Idce0333f31648563c8b779687487037ff2d01e6c

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -290,7 +290,7 @@ static int snd_usb_create_streams(struct snd_usb_audio *chip, int ctrlif)
 		break;
 	}
 	}
-	switch_set_state(usbaudiosdev, 1);
+	switch_set_state(usbaudiosdev, 2);
 	return 0;
 }
 
