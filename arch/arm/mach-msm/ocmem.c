89959ab7a2f053358bf2c35d940e430741f8ad51
Neeti Desai <neetid@codeaurora.org>
2014-04-28 17:12:27 -0700

    msm: ocmem: Call restore_sec_config() after every power collapse
    As the QRIB is enabled, after every power collapse of ocmem, the
    QRIB is reset to the maximum security state. Due to this the apps
    processor cannot access the ocmem register space.
    
    The restore_sec_config() scm call needs to be called before any
    register is accessed after a power collapse to ensure that the
    security configuration is restored. The clocks need to be turned
    on before every call that is made into TZ to ensure that there
    is no power collapse.
    
    CRs-Fixed: 651324, 660089
    Change-Id: Ieb6b635142057e219de29e811c93749f6500cec5
    Signed-off-by: Neeti Desai <neetid@codeaurora.org>

--- arch/arm/mach-msm/ocmem.c
+++ arch/arm/mach-msm/ocmem.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -795,6 +795,7 @@ static int __devinit msm_ocmem_probe(struct platform_device *pdev)
 	struct device   *dev = &pdev->dev;
 	struct clk *ocmem_core_clk = NULL;
 	struct clk *ocmem_iface_clk = NULL;
+	int rc;
 
 	ocmem_core_clk = devm_clk_get(dev, "core_clk");
 
@@ -835,10 +836,23 @@ static int __devinit msm_ocmem_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, ocmem_pdata);
 
+	rc = ocmem_enable_core_clock();
+	if (rc < 0)
+		goto core_clk_fail;
+
+	rc = ocmem_enable_iface_clock();
+	if (rc < 0)
+		goto iface_clk_fail;
+
 	/* Parameter to be updated based on TZ */
 	/* Allow the OCMEM CSR to be programmed */
-	if (ocmem_enable_sec_program(OCMEM_SECURE_DEV_ID))
+	if (ocmem_restore_sec_program(OCMEM_SECURE_DEV_ID)) {
+		ocmem_disable_iface_clock();
+		ocmem_disable_core_clock();
 		return -EBUSY;
+	}
+	ocmem_disable_iface_clock();
+	ocmem_disable_core_clock();
 
 	if (ocmem_debugfs_init(pdev))
 		dev_err(dev, "ocmem: No debugfs node available\n");
@@ -866,6 +880,11 @@ static int __devinit msm_ocmem_probe(struct platform_device *pdev)
 	probe_done = true;
 	dev_dbg(dev, "initialized successfully\n");
 	return 0;
+
+iface_clk_fail:
+	ocmem_disable_core_clock();
+core_clk_fail:
+	return rc;
 }
 
 static int __devexit msm_ocmem_remove(struct platform_device *pdev)
