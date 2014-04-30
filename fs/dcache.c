39e79f9437e95e6fd1eac8efc17366d107f9d36f
Al Viro <viro@zeniv.linux.org.uk>
2014-04-29 23:40:14 -0400

    new helper: dentry_free()
    The part of old d_free() that dealt with actual freeing of dentry.
    Taken out of dentry_kill() into a separate function.
    
    Git-repo: https://github.com/torvalds/linux.git
    Git-commit: b4f0354e968f5fabd39bc85b99fedae4a97589fe
    Change-Id: I4b16554b6f6acc558d299ca3282eebf93612d8a9
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Ankit Jain <jankit@codeaurora.org>

--- fs/dcache.c
+++ fs/dcache.c
@@ -207,6 +207,15 @@ static void __d_free(struct rcu_head *head)
 	kmem_cache_free(dentry_cache, dentry); 
 }
 
+static void dentry_free(struct dentry *dentry)
+{
+    /* if dentry was never visible to RCU, immediate free is OK */
+    if (!(dentry->d_flags & DCACHE_RCUACCESS))
+        __d_free(&dentry->d_u.d_rcu);
+    else
+        call_rcu(&dentry->d_u.d_rcu, __d_free);
+}
+
 /*
  * no locks, please.
  */
@@ -218,11 +227,7 @@ static void d_free(struct dentry *dentry)
 	if (dentry->d_op && dentry->d_op->d_release)
 		dentry->d_op->d_release(dentry);
 
-	/* if dentry was never visible to RCU, immediate free is OK */
-	if (!(dentry->d_flags & DCACHE_RCUACCESS))
-		__d_free(&dentry->d_u.d_rcu);
-	else
-		call_rcu(&dentry->d_u.d_rcu, __d_free);
+	dentry_free(dentry);
 }
 
 void take_dentry_name_snapshot(struct name_snapshot *name, struct dentry *dentry)
