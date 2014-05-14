83f7008a3a7afbfcba92bccaee511adcd3363d6a
Yue Ma <yuem@codeaurora.org>
2014-05-14 15:49:54 -0700

    cnss: Add pre-allocation feature for CNSS platform driver
    Make WCNSS pre-allocation feature code as a module which can be shared
    by CNSS and WCNSS platform drivers. It will reserve chunks of memory at
    booting time so that WLAN host driver can directly use them in case that
    kernel cannot allocate large memory immediately.
    
    CRs-fixed: 662637
    Signed-off-by: Yue Ma <yuem@codeaurora.org>
    
    cnss: Update pre-allocation table
    
    Update pre-allocation table in order to fulfill latest WLAN host driver.
    Also add WARN_ON to print trace information if memory request fails.
    
    CRs-fixed: 662637
    Signed-off-by: Yue Ma <yuem@codeaurora.org>
    
    cnss: Update pre-allocation table
    
    Update pre-allocation table in order to fulfill latest WLAN host driver's
    scan request since it may fail to get a large chuck of memory when all
    the pre-allocated memory are in use. Also remove WARN_ON since it already
    prints errors when memory request fails.
    
    CRs-Fixed: 662637
    Signed-off-by: Yue Ma <yuem@codeaurora.org>
    
    cnss: Update pre-allocation table
    
    Update pre-allocation table in order to fulfill latest WLAN host driver.
    This will avoid failing to get enough large chucks of memory when other
    same size pre-allocated memory are in use during driver load.
    
    Change-Id: I1b6dd9a7a5352e8828e2adf6b23ef523c29a2e7e
    CRs-fixed: 752152
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -2959,19 +2959,12 @@ static struct platform_driver wcnss_wlan_driver = {
 
 static int __init wcnss_wlan_init(void)
 {
-	int ret = 0;
-
 	platform_driver_register(&wcnss_wlan_driver);
 	platform_driver_register(&wcnss_wlan_ctrl_driver);
 	platform_driver_register(&wcnss_ctrl_driver);
 	register_pm_notifier(&wcnss_pm_notifier);
-#ifdef CONFIG_WCNSS_MEM_PRE_ALLOC
-	ret = wcnss_prealloc_init();
-	if (ret < 0)
-		pr_err("wcnss: pre-allocation failed\n");
-#endif
 
-	return ret;
+	return 0;
 }
 
 static void __exit wcnss_wlan_exit(void)
@@ -2982,9 +2975,6 @@ static void __exit wcnss_wlan_exit(void)
 		penv = NULL;
 	}
 
-#ifdef CONFIG_WCNSS_MEM_PRE_ALLOC
-	wcnss_prealloc_deinit();
-#endif
 	unregister_pm_notifier(&wcnss_pm_notifier);
 	platform_driver_unregister(&wcnss_ctrl_driver);
 	platform_driver_unregister(&wcnss_wlan_ctrl_driver);
