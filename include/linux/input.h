fa3d6f4a478a80660e4707c34a2e0173c39ac80c
Danesh M <daneshm90@gmail.com>
2014-05-20 22:59:33 -0700

    OPPO-MSM8974 : Use explicit KEY_DOUBLE_TAP to handle in userspace
    Multiple taps get queued and result in the device going on -> off.
    Avoid that by sending the key to userspace where display state checks are
    performed
    
    Change-Id: Ib6315d2123cb34d70cca3b045b8d32abc9e044ab

--- include/linux/input.h
+++ include/linux/input.h
@@ -479,6 +479,7 @@ struct input_keymap_entry {
 #define KEY_GESTURE_V		252
 #define KEY_GESTURE_LTR		253
 #define KEY_GESTURE_GTR		254
+#define KEY_DOUBLE_TAP      255
 
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
