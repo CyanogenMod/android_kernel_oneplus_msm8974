07707ea80b41207590bb4eebafe0236bc81bac52
Dhaval Patel <pdhaval@codeaurora.org>
2014-06-05 17:59:17 -0700

    msm: mdss: handle race condition between ov_off and ov_commit
    Handle race condition between ov_off and ov_commit by
    acquiring the ov_lock in off API. This gives the exclusive
    behavior between ov_off and ov_commit call.
    
    Change-Id: Iaa13f9e5991a53feff12b8efaec497cae49d28d1
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -3297,6 +3297,8 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 		goto ctl_stop;
 	}
 
+	mutex_lock(&mdp5_data->ov_lock);
+
 	mdss_mdp_overlay_free_fb_pipe(mfd);
 
 	mixer = mdss_mdp_mixer_get(mdp5_data->ctl, MDSS_MDP_MIXER_MUX_LEFT);
@@ -3310,6 +3312,7 @@ static int mdss_mdp_overlay_off(struct msm_fb_data_type *mfd)
 	mutex_lock(&mdp5_data->list_lock);
 	need_cleanup = !list_empty(&mdp5_data->pipes_cleanup);
 	mutex_unlock(&mdp5_data->list_lock);
+	mutex_unlock(&mdp5_data->ov_lock);
 
 	if (need_cleanup) {
 		pr_debug("cleaning up pipes on fb%d\n", mfd->index);
@@ -3360,6 +3363,8 @@ ctl_stop:
 		    }
 	    }
 	}
+
+	mutex_unlock(&mdp5_data->ov_lock);
 
 	/* Release the last reference to the runtime device */
 	rc = pm_runtime_put(&mfd->pdev->dev);
