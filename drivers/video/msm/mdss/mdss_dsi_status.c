4e3ed1e9b7e467924167877041ce6d2ecb7a6bf8
Charles Bin <cbin@codeaurora.org>
2014-06-27 11:17:33 -0700

    mdss: dsi: Fix null dereferences
    Fix potential null dereferences in driver.
    
    Change-Id: Idd8d4eb16bb1b9562da1f5225d91cf90d8230bfe
    Signed-off-by: Charles Bin <cbin@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_status.c
+++ drivers/video/msm/mdss/mdss_dsi_status.c
@@ -82,6 +82,11 @@ static int fb_event_callback(struct notifier_block *self,
 				struct dsi_status_data, fb_notifier);
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
 
+	if (!evdata) {
+		pr_err("%s: event data not available\n", __func__);
+		return NOTIFY_BAD;
+	}
+
 	pdata->mfd = evdata->info->par;
 	ctrl_pdata = container_of(dev_get_platdata(&pdata->mfd->pdev->dev),
 				struct mdss_dsi_ctrl_pdata, panel_data);
@@ -94,7 +99,7 @@ static int fb_event_callback(struct notifier_block *self,
 		return NOTIFY_DONE;
 	}
 
-	if (event == FB_EVENT_BLANK && evdata) {
+	if (event == FB_EVENT_BLANK) {
 		int *blank = evdata->data;
 		struct dsi_status_data *pdata = container_of(self,
 				struct dsi_status_data, fb_notifier);
