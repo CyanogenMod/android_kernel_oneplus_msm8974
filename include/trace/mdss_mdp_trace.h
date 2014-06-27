4420d2b12bd2ac05bca30c4e8111072a7fc90dab
Ethan Chen <intervigil@gmail.com>
2014-06-27 15:02:18 -0700

    msm: mdss: Fix compilation
    Include mdss_mdp_trace header file in trace directory to
    avoid compile errors when compiling standalone kernel image.
    
    Change-Id: I047183ecd280ea1f931007b0e1e3c5445c60b1ab

--- include/trace/mdss_mdp_trace.h
+++ include/trace/mdss_mdp_trace.h
@@ -22,7 +22,7 @@
 #define TRACE_INCLUDE_FILE mdss_mdp_trace
 
 #include <linux/tracepoint.h>
-#include "mdss_mdp.h"
+#include "../../drivers/video/msm/mdss/mdss_mdp.h"
 
 DECLARE_EVENT_CLASS(mdp_sspp_template,
 	TP_PROTO(struct mdss_mdp_pipe *pipe),
