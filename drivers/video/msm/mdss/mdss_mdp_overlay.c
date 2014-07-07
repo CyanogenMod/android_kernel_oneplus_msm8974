9699d40eb5dcefec2353889ef3e75636c3f9baa1
Steve Kondik <shade@chemlab.org>
2014-07-06 18:40:11 -0700

    video: mdss: Fix various issues with pipe allocation
     * Make sure clocks are on when freeing
     * Remove stale flag
    
    Change-Id: I8f1b3da4e4bbb724ed298d2904be189503c23d1c

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -462,6 +462,13 @@ int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 
 		pipe = mdss_mdp_pipe_alloc(mixer, pipe_type);
 
+		/* RGB pipes can be used instead of DMA */
+		if ((req->pipe_type == PIPE_TYPE_AUTO) && !pipe &&
+			(pipe_type == MDSS_MDP_PIPE_TYPE_DMA)) {
+			pipe_type = MDSS_MDP_PIPE_TYPE_RGB;
+			pipe = mdss_mdp_pipe_alloc(mixer, pipe_type);
+		}
+
 		/* VIG pipes can also support RGB format */
 		if ((req->pipe_type == PIPE_TYPE_AUTO) && !pipe &&
 			(pipe_type == MDSS_MDP_PIPE_TYPE_RGB)) {
@@ -1404,7 +1411,6 @@ static int mdss_mdp_overlay_queue(struct msm_fb_data_type *mfd,
 		pr_warn("Unexpected buffer queue to a solid fill pipe\n");
 
 	flags = (pipe->flags & MDP_SECURE_OVERLAY_SESSION);
-	flags |= (pipe->flags & MDP_SECURE_DISPLAY_OVERLAY_SESSION);
 
 	src_data = &pipe->back_buf;
 	if (src_data->num_planes) {
