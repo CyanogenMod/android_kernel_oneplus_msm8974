1bd4a37a6dfff6330b801c232e4bfdf170bab907
andip71 <andreasp@gmx.de>
2014-09-16 06:49:08 +0200

    mmc/core: disable crc to improve performance

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -70,7 +70,7 @@ static struct workqueue_struct *workqueue;
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-bool use_spi_crc = 1;
+bool use_spi_crc = 0;
 module_param(use_spi_crc, bool, 0);
 
 /*
