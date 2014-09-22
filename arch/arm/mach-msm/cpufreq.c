fbb4bcd820ea3d5c238e08e847c1366b59181719
anarkia1976 <stefano.villa1976@gmail.com>
2014-09-22 06:13:12 +0200

    Voltage Control: adapted for the new devfreq drivers

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -382,8 +382,11 @@ static struct cpufreq_frequency_table *cpufreq_parse_dt(struct device *dev,
 	if (!dts_freq_table)
 		return ERR_PTR(-ENOMEM);
 
-	for (i = 0, j = 0; i < nf; i++, j += 3)
-		dts_freq_table[i].frequency = data[j];
+	dts_freq_table = *ftbl;
+
+	for (i = 0; i < nf; i++)
+		dts_freq_table[i].frequency = data[i];
+
 	dts_freq_table[i].frequency = CPUFREQ_TABLE_END;
 #endif
 
