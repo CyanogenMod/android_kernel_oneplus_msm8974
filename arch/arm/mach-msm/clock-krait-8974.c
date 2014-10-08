51a8cb24bbf685641b68fd0cf83be5646364d2b2
andip71 <andreasp@gmx.de>
2014-10-08 13:16:23 +0200

    clock-krait-8974: Expose PVS level via sysfs

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -35,6 +35,11 @@
 #include "clock-krait.h"
 #include "clock.h"
 
+#ifdef CONFIG_PVS_LEVEL_INTERFACE
+int pvs_level = -1;
+module_param(pvs_level, int, S_IRUGO); 
+#endif
+
 /* Clock inputs coming into Krait subsystem */
 DEFINE_FIXED_DIV_CLK(hfpll_src_clk, 1, NULL);
 DEFINE_FIXED_DIV_CLK(acpu_aux_clk, 2, NULL);
@@ -504,6 +509,10 @@ static void get_krait_bin_format_b(struct platform_device *pdev,
 		*pvs = 0;
 		*svs_pvs = -1;
 	}
+
+#ifdef CONFIG_PVS_LEVEL_INTERFACE
+	pvs_level = *pvs;
+#endif
 
 	dev_info(&pdev->dev, "PVS version: %d\n", *pvs_ver);
 
