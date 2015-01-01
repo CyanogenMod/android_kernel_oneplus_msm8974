1d2bc113570adf8995985e3f14d402141059a297
Steve Kondik <steve@cyngn.com>
2015-01-01 14:54:26 -0800

    video: mdss: Fix SRE threshold code
     * SRE was not being enabled when the threshold was crossed. Fix
       this logic. Also add a sysfs knob to set the threshold.
    
    Change-Id: Iac5cf22d127938c89c26f0b27341480e8430c8ee

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -156,32 +156,31 @@ int mdss_dsi_panel_update_sre(struct mdss_panel_data *pdata, u32 bl_level)
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
 	struct mdss_panel_info *pinfo = NULL;
 
+	int sre = 0;
+	int cabc = 0;
+
 	if (pdata == NULL)
 		return -EINVAL;
 
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata, panel_data);
 	pinfo = &(ctrl_pdata->panel_data.panel_info);
 
-	if (!pinfo->cabc_mode || !pinfo->cabc_bl_max || !pinfo->sre_bl_threshold)
+	if (!pinfo->cabc_mode)
 		return 0;
 
 	// disable CABC above some backlight value since it's not effective at high
 	// brightness. optionally enable SRE when it's even higher to melt faces off.
-	if ((!pinfo->cabc_active || pinfo->sre_active) && bl_level <= pinfo->cabc_bl_max) {
-		pinfo->cabc_active = true;
-		pinfo->sre_active = false;
+	cabc = pinfo->cabc_mode &&
+			(pinfo->cabc_bl_max <= 0 || bl_level <= pinfo->cabc_bl_max);
+	sre = pinfo->sre_enabled &&
+			(pinfo->sre_bl_threshold <= 0 || bl_level > pinfo->sre_bl_threshold);
+
+	if (cabc != pinfo->cabc_active || sre != pinfo->sre_active) {
+		pinfo->cabc_active = cabc;
+		pinfo->sre_active = sre;
 		ret = mdss_dsi_update_cabc_level(ctrl_pdata);
-	} else if (bl_level > pinfo->cabc_bl_max) {
-		if (pinfo->sre_enabled && !pinfo->sre_active && bl_level >= pinfo->sre_bl_threshold) {
-			pinfo->cabc_active = false;
-			pinfo->sre_active = true;
-			ret = mdss_dsi_update_cabc_level(ctrl_pdata);
-		} else if (pinfo->cabc_active || pinfo->sre_active) {
-			pinfo->cabc_active = false;
-			pinfo->sre_active = false;
-			ret = mdss_dsi_update_cabc_level(ctrl_pdata);
-		}
 	}
+
 	return ret;
 }
 
