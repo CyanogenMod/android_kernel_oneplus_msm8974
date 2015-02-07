33e9d08ccd36f55b7b775e5b65923e1e899b3f8c
Ashray Kulkarni <ashrayk@codeaurora.org>
2015-02-06 19:37:44 -0800

    msm: vidc: replace resurrect_fw with unload_fw
    resurrect_fw() unloads and reloads firmware after 5 seconds.
    Opening new sessions during these 5 seconds fails. This
    commit replaces resurrect_fw() with unload_fw ensuring current
    session is unloaded cleanly.
    
    Change-Id: Iaffc7b0e752f488182cd15db2913742416d3662d
    Signed-off-by: Ashray Kulkarni <ashrayk@codeaurora.org>
    Signed-off-by: Balamurugan Alagarsamy <balaga@codeaurora.org>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -3775,7 +3775,6 @@ static void venus_hfi_unload_fw(void *dev)
 		return;
 	}
 	if (device->resources.fw.cookie) {
-		flush_workqueue(device->vidc_workq);
 		flush_workqueue(device->venus_pm_workq);
 		subsystem_put(device->resources.fw.cookie);
 		venus_hfi_interface_queues_release(dev);
