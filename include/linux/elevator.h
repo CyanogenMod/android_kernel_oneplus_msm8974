a71d11193af4678f808ea6f2830bc493c95174eb
anarkia1976 <stefano.villa1976@gmail.com>
2015-02-10 08:52:40 +0100

    block: scheduler: add SIOPLUS
    Conflicts:
    	block/Kconfig.iosched
    
    Conflicts:
    	arch/arm/configs/lightning_bacon_defconfig
    	block/Kconfig.iosched

--- include/linux/elevator.h
+++ include/linux/elevator.h
@@ -22,6 +22,7 @@ typedef void (elevator_bio_merged_fn) (struct request_queue *,
 typedef int (elevator_dispatch_fn) (struct request_queue *, int);
 
 typedef void (elevator_add_req_fn) (struct request_queue *, struct request *);
+typedef int (elevator_queue_empty_fn) (struct request_queue *);
 typedef int (elevator_reinsert_req_fn) (struct request_queue *,
 					struct request *);
 typedef bool (elevator_is_urgent_fn) (struct request_queue *);
@@ -55,6 +56,7 @@ struct elevator_ops
 	elevator_activate_req_fn *elevator_activate_req_fn;
 	elevator_deactivate_req_fn *elevator_deactivate_req_fn;
 
+	elevator_queue_empty_fn *elevator_queue_empty_fn;
 	elevator_completed_req_fn *elevator_completed_req_fn;
 
 	elevator_request_list_fn *elevator_former_req_fn;
