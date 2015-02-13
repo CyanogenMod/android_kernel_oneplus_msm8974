461853d491e430118efeebfeb95e1b9640bad3fd
Mitchel Humpherys <mitchelh@codeaurora.org>
2015-02-12 21:28:07 -0800

    ion: return a fresh sg_table from ion_map_dma_buf
    DMA mappers often (always?) modify the scatterlist they are
    given (e.g. dma_address and dma_length) when their dma_map_ops.map_sg
    operation is called.  Currently Ion always returns the same sg_table
    when it's dma_buf_ops.map_dma_buf operation is called.  The result is
    that when Ion is used as a DMA buf exporter to multiple clients (a
    common scenario during zero-copy buffer-sharing) those clients will be
    stomping all over each other since they're both trying to put their
    mappings into the same sg_table.
    
    Fix this by always duplicating the buffer's sg_table in ion_map_dma_buf
    and returning the duplicate.  Each buffer will still always have a
    single "master" sg_table that should never be exported to the DMA buf
    framework.
    
    Test case (pseudo-code):
    
        handle = ion_alloc();
        dbuf = ion_share_dma_buf(handle);
        attach = dma_buf_attach(dbuf);
        table1 = dma_buf_map_attachment(attach);
        table2 = dma_buf_map_attachment(attach);
        dma_map_sg(table1);
        table2 = dma_buf_map_attachment(attach);
        dma_map_sg(table2);
        /* table1 is now hosed */
        check_mappings(table1); /* KABOOM */
    
    Change-Id: Ifefa06ecd464c00819b4ae7ac32ad37f1491373b
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- drivers/gpu/ion/ion_priv.h
+++ drivers/gpu/ion/ion_priv.h
@@ -50,7 +50,11 @@ struct ion_buffer *ion_handle_buffer(struct ion_handle *handle);
  * @kmap_cnt:		number of times the buffer is mapped to the kernel
  * @vaddr:		the kenrel mapping if kmap_cnt is not zero
  * @dmap_cnt:		number of times the buffer is mapped for dma
- * @sg_table:		the sg table for the buffer if dmap_cnt is not zero
+ * @sg_table:		the sg table for the buffer.  Note that if you need
+ *			an sg_table for this buffer, you should likely be
+ *			using Ion as a DMA Buf exporter and using
+ *			dma_buf_map_attachment rather than trying to use this
+ *			field directly.
  * @pages:		flat array of pages in the buffer -- used by fault
  *			handler and only valid for buffers that are faulted in
  * @vmas:		list of vma's mapping this buffer
