2f36fdcc1625240bc15c018fbcbfee0957f7515f
Zachariah <zkennedy87@gmail.com>
2015-06-13 18:34:09 -0400

    cpufreq: impulse: added new governor from Hybrid Kernel
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -400,6 +400,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 
