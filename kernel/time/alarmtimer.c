f2e56d7b2e208f8095ffcb45801ba17b2fdba63c
Ricardo Cerqueira <ricardo@cyngn.com>
2015-06-17 02:31:19 +0100

    Revert "alarmtimer: set power off alarm to be triggered on time"
    This reverts commit 42d2a8fe536de69153c0cfaaed50089f76f01f13.

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -77,7 +77,7 @@ void power_on_alarm_init(void)
 	rtc_tm_to_time(&rt, &alarm_time);
 
 	if (alarm_time)
-		power_on_alarm = alarm_time;
+		power_on_alarm = alarm_time + ALARM_DELTA;
 	else
 		power_on_alarm = 0;
 }
@@ -117,7 +117,9 @@ void set_power_on_alarm(long secs, bool enable)
 	 *to power up the device before actual alarm
 	 *expiration
 	 */
-	if (alarm_time <= rtc_secs)
+	if ((alarm_time - ALARM_DELTA) > rtc_secs)
+		alarm_time -= ALARM_DELTA;
+	else
 		goto disable_alarm;
 
 	rtc_time_to_tm(alarm_time, &alarm.time);
