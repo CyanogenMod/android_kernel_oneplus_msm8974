715e86e614d0c7fca0024a4ee86194661ba955d9
maxwen <max.weninger@gmail.com>
2015-07-05 21:31:15 +0200

    synaptics: Fix find7s HW version checks
    Change-Id: I166720d39ec12d2552f6904e4886cc08de933771

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -1089,7 +1089,7 @@ static void vk_calculate_area(void)  //added by liujun
 	printk("[syna]maxx=%d,maxy=%d,vkh=%d\n",syna_ts_data->sensor_max_x,syna_ts_data->sensor_max_y,syna_ts_data->virtual_key_height);
 
 	syna_ts_data->vk_prop_width = LCD_MULTI_RATIO(190);
-	if (get_pcb_version() <= HW_VERSION__20) {
+	if (get_pcb_version() < HW_VERSION__20) {
 		syna_ts_data->vk_prop_center_y = LCD_MULTI_RATIO(1974);
 		syna_ts_data->vk_prop_height = LCD_MULTI_RATIO(120);
 	} else {
@@ -1169,7 +1169,7 @@ static int get_virtual_key_button(int x, int y)
 	int i;
 	int lcdheight = LCD_MAX_Y;
 
-	if (get_pcb_version() > HW_VERSION__20)
+	if (get_pcb_version() >= HW_VERSION__20)
 		lcdheight = LCD_MAX_Y_FIND7S;
 
 	if (y <= lcdheight)
@@ -2084,7 +2084,7 @@ static ssize_t synaptics_rmi4_baseline_data(char *buf, bool savefile)
 	num_read_chars += sprintf(&(buf[num_read_chars]), " trx-to-trx raw readback: %x  %x  %x  %x  %x  %x \n", data_buf[0], data_buf[1], data_buf[2], data_buf[3], data_buf[4], data_buf[5]);
 #ifdef CONFIG_MACH_FIND7
 	//mingqiang.guo@phone.bsp 2014-5-30  modify  must clear not use channel , other it will wrong
-	if (get_pcb_version() <= HW_VERSION__20) { //find7
+	if (get_pcb_version() < HW_VERSION__20) { //find7
 		if ((data_buf[0] == 0) && (data_buf[1] == 0) && (data_buf[2] == 0)
 			&& ((data_buf[3] & 0x0f) == 0) && ((data_buf[5] & 0x7f) == 0)) {
 			if ((syna_ts_data->vendor_id == TP_VENDOR_TPK) && (data_buf[4] == 0))
@@ -2157,7 +2157,7 @@ static ssize_t synaptics_rmi4_baseline_data(char *buf, bool savefile)
 	//mingqiang.guo@phone.bsp modify  only 13077 wintk and tpk use different channels,  tpk: data_buf[4]==0xff wintek :  data_buf[4]==0xfe
 	print_ts(TS_DEBUG, "%d: syna_ts_data->vendor_id=%d,data_buf[4]=%d\n", __LINE__, syna_ts_data->vendor_id, data_buf[4]);
 
-	if (get_pcb_version() <= HW_VERSION__20) { //find7
+	if (get_pcb_version() < HW_VERSION__20) { //find7
 		if ((data_buf[0] == 0xff) && (data_buf[1] == 0xff) && (data_buf[2] == 0xff)
 			&& ((data_buf[3] & 0x0f) == 0x0f)  && ((data_buf[5] & 0x7f) == 0x7f)) {
 			if ((syna_ts_data->vendor_id == TP_VENDOR_TPK) && (data_buf[4] == 0xff))
@@ -4315,7 +4315,7 @@ static void synaptics_rmi4_get_vendorid(struct synaptics_rmi4_data *rmi4_data) {
 	vendor_id = synaptics_rmi4_get_vendorid1(gpio_get_value(rmi4_data->id_gpio),
 			gpio_get_value(rmi4_data->wakeup_gpio), 0);
 #else
-	if (get_pcb_version() >= HW_VERSION__21)
+	if (get_pcb_version() >= HW_VERSION__20)
 		vendor_id = synaptics_rmi4_get_vendorid1(gpio_get_value(rmi4_data->id_gpio),
 				gpio_get_value(rmi4_data->wakeup_gpio), 0);
 	else
@@ -4420,14 +4420,12 @@ static int __devinit synaptics_rmi4_probe(struct i2c_client *client,
 	rmi4_data->reset_device = synaptics_rmi4_reset_device;
 
 	//init sensor size
-	if (get_pcb_version() <= HW_VERSION__20) {
+	if (get_pcb_version() < HW_VERSION__20) {
 		rmi4_data->virtual_key_height = 114;
 		rmi4_data->sensor_max_x = LCD_MAX_X;
 		rmi4_data->sensor_max_y = LCD_MAX_Y+120;
 		syna_lcd_ratio1 = 100;
 		syna_lcd_ratio2 = 100;
-		if (get_pcb_version() == HW_VERSION__20)
-			syna_lcd_ratio1 = 135;
 	} else {
 		rmi4_data->virtual_key_height = 142;
 		rmi4_data->sensor_max_x = LCD_MAX_X_FIND7S;
