7d450e2c5ff840362da3907b32383d25494771d0
Klozz <xxx.reptar.rawrr.xxx@gmail.com>
2015-07-08 09:57:49 +0530

    Enhance power efficiency: Series of patches from NVIDIA on some criti… …cal blocking code paths to allow the CPU to relax instead of busy spinning thus lowering the load_average and lowering power consumption.
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    
    Conflicts:
    	kernel/sched/core.c
    
    Change-Id: I9d1752a29f6c7448d64284ac1acd2c3f70d240b8
    
    Conflicts:
    	kernel/sched/core.c
    
    Conflicts:
    	kernel/sched/core.c

--- arch/arm/include/asm/processor.h
+++ arch/arm/include/asm/processor.h
@@ -2,6 +2,8 @@
  *  arch/arm/include/asm/processor.h
  *
  *  Copyright (C) 1995-1999 Russell King
+ * 
+ * Copyright (c) 2014, NVIDIA CORPORATION. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -126,5 +128,7 @@ static inline void prefetch(const void *ptr)
 #define HAVE_ARCH_PICK_MMAP_LAYOUT
 
 #endif
+
+#include <asm-generic/processor.h>
 
 #endif /* __ASM_ARM_PROCESSOR_H */
