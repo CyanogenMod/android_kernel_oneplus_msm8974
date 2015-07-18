a9989ec4c81274a740e9307a2484dbc3597ffd05
Michael Bestas <mikeioannina@gmail.com>
2015-07-18 14:37:24 +0300

    media: Apply VENUS_BUFFER_SIZE hack only on oneplus
    Change-Id: I622c74a861206f06d90cf6d4379e6f85a5078499

--- include/media/msm_media_info.h
+++ include/media/msm_media_info.h
@@ -88,7 +88,7 @@ static inline unsigned int VENUS_EXTRADATA_SIZE(int width, int height)
 	 * In the future, calculate the size based on the w/h but just
 	 * hardcode it for now since 8K satisfies all current usecases.
 	 */
-#if 0
+#ifndef CONFIG_ONEPLUS_CAMERA
 	return 8 * 1024;
 #else
 	// BREAK ALL THE tHINGS
