e22d859e78613a177d1c57b59416cc7f1e81934e
anarkia1976 <stefano.villa1976@gmail.com>
2015-07-21 21:35:31 +0200

    fs: support task's adj rbtree
    Add (or del) a task to (or from) task's adj rbtree
    when its oom_score_adj is modified.
    
    Change-Id: I8a9490c6f30f42f6e4ed8f7fa9249f6da6177ae2
    Signed-off-by: Hong-Mei Li <a21834@motorola.com>
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/701206
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -949,7 +949,9 @@ static ssize_t oom_adjust_write(struct file *file, const char __user *buf,
 	printk_once(KERN_WARNING "%s (%d): /proc/%d/oom_adj is deprecated, please use /proc/%d/oom_score_adj instead.\n",
 		  current->comm, task_pid_nr(current), task_pid_nr(task),
 		  task_pid_nr(task));
+	delete_from_adj_tree(task);
 	task->signal->oom_adj = oom_adjust;
+	add_2_adj_tree(task);
 	/*
 	 * Scale /proc/pid/oom_score_adj appropriately ensuring that a maximum
 	 * value is always attainable.
@@ -1044,7 +1046,10 @@ static ssize_t oom_score_adj_write(struct file *file, const char __user *buf,
 		goto err_sighand;
 	}
 
+	delete_from_adj_tree(task);
 	task->signal->oom_score_adj = oom_score_adj;
+	add_2_adj_tree(task);
+
 	if (has_capability_noaudit(current, CAP_SYS_RESOURCE))
 		task->signal->oom_score_adj_min = oom_score_adj;
 	trace_oom_score_adj_update(task);
