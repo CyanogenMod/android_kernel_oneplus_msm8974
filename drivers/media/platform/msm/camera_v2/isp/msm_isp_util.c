b3b9ba01fa1218b3f524928ad2d23c563cb57b55
Sultanxda <sultanxda@gmail.com>
2015-07-24 23:39:44 -0700

    msm: camera: isp: Silence IRQ status log-spam
    Change-Id: I98a265ad32a538e9ef1809afa5e64334fc44658c

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -1200,7 +1200,7 @@ irqreturn_t msm_isp_process_irq(int irq_num, void *data)
 	vfe_dev->hw_info->vfe_ops.irq_ops.
 		read_irq_status(vfe_dev, &irq_status0, &irq_status1);
 	if ((irq_status0 == 0) && (irq_status1 == 0)) {
-		pr_err_ratelimited("%s: irq_status0 & 1 are both 0\n",
+		ISP_DBG("%s: irq_status0 & 1 are both 0\n",
 			__func__);
 		return IRQ_HANDLED;
 	}
