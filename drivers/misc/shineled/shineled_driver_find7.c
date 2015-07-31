ef9183ce938828bb307eee36871be1537ed99a4a
Michael Bestas <mikeioannina@gmail.com>
2015-07-31 21:49:57 +0300

    Revert "misc: shineled: Update driver for Find7S"
    * We will disable this properly in DT
    
    This reverts commit 20d16cdbda149f2359334a37f6b1afb603a95955.
    
    Change-Id: I43e81beda95dbd2b0ebf1b0276953004c84d68fd

--- drivers/misc/shineled/shineled_driver_find7.c
+++ drivers/misc/shineled/shineled_driver_find7.c
@@ -1166,20 +1166,8 @@ static int __init SN3193_dev_init(void)
 		ret = -ENOMEM;
 		goto err_exit;
 	}
-/*OPPO yuyi 2014-02-20 modified begin for SN3193 would not be used*/
-#ifndef VENDOR_EDIT
 	pr_info("yanghai shineld driver init\n");
 	return i2c_add_driver(&SN3193_driver);
-#else
-	if(get_pcb_version() < HW_VERSION__20) {
-		pr_info("SN3193 shineld driver init\n");
-		return i2c_add_driver(&SN3193_driver);
-	} else {
-		pr_info("SN3193 13097 forbiden shineld driver init\n");
-		return -1;
-	};
-#endif
-/*OPPO yuyi 2014-02-20 modified end for SN3193 would not be used*/
 err_exit:
 	return ret;
 }
