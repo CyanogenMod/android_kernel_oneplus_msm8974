d7f2e5c69d04a4d7fbb2c09702a159031e071648
Srinivasarao P <spathi@codeaurora.org>
2015-08-10 14:27:24 +0530

    arm: mm: fix bootup bug with odd-section boundry in memory hole.
    Bootup failure is seen when CONFIG_FREE_PAGES_RDONLY is enabeld and
    there is odd-section start boundary in memory hole.
    When the static mapping is completed, fill_pmd_gaps() is called and
    sees there's an entry for odd-section boundary and pmd_empty_section_gap()
    is again called with the same end address. This triggers BUG() inside
    vm_area_add_early() and prevents the device from booting up.
    
    Change-Id: Ief7ac4c157746085feddc5fc32df15d567583f52
    CRs-fixed: 871975
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -1552,7 +1552,7 @@ static void __init remap_pages(void)
 		pmd_t *pmd = NULL;
 		unsigned long next;
 		unsigned long pfn = __phys_to_pfn(phys_start);
-		bool fixup = false;
+		bool fixup = false, end_fixup = false;
 		unsigned long saved_start = addr;
 
 		if (phys_end > arm_lowmem_limit)
@@ -1570,8 +1570,10 @@ static void __init remap_pages(void)
 			pmd++;
 		}
 
-		if (end & SECTION_SIZE)
+		if (end & SECTION_SIZE) {
+			end_fixup = true;
 			pmd_empty_section_gap(end);
+		}
 #endif
 
 		do {
@@ -1594,6 +1596,10 @@ static void __init remap_pages(void)
 			 */
 			pmd = pmd_off_k(saved_start);
 			pmd[0] = pmd[1] & ~1;
+		}
+		if (end_fixup) {
+			pmd = pmd_off_k(end);
+			pmd[1] = pmd[0] & ~1;
 		}
 	}
 }
