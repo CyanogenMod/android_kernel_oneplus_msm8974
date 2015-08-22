0f56f31835d8c3277b26b4ffc70091baeaeb6ec0
Michael Bestas <mikeioannina@gmail.com>
2015-08-22 16:31:40 +0300

    Revert "mdss: dsi: Don't make lack of reset GPIO a fatal failure yet"
    * Not needed
    
    This reverts commit 616c7c4ca9a694e68038cd61f6d3c4afd698ab9b.
    
    Change-Id: I13eca39ab1d714d82cecce68afaeb412a2cc8421

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -257,6 +257,7 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	if (!gpio_is_valid(ctrl_pdata->rst_gpio)) {
 		pr_debug("%s:%d, reset line not configured\n",
 			   __func__, __LINE__);
+		return rc;
 	}
 
 	pr_debug("%s: enable = %d\n", __func__, enable);
@@ -314,10 +315,8 @@ int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 			gpio_set_value((ctrl_pdata->disp_en_gpio), 0);
 			gpio_free(ctrl_pdata->disp_en_gpio);
 		}
-		if (gpio_is_valid(ctrl_pdata->rst_gpio)) {
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
-			gpio_free(ctrl_pdata->rst_gpio);
-		}
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		gpio_free(ctrl_pdata->rst_gpio);
 		if (gpio_is_valid(ctrl_pdata->mode_gpio))
 			gpio_free(ctrl_pdata->mode_gpio);
 	}
