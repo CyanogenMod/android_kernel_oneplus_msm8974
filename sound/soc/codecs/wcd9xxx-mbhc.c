a7674121eb5b38f82908a4380ca90386220d74ff
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2015-08-26 18:44:17 -0700

    ASoC: wcd9xxx-mbhc: Avoid multiple insertion/removals for aux cable
    Avoid multiple insertion/removal events to user-space when 3-pole
    aux cable is inserted into headset jack.
    
    Change-Id: I0d4996d2e3f1fd247e4fcb91760c4b1646bd77a9
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>

--- sound/soc/codecs/wcd9xxx-mbhc.c
+++ sound/soc/codecs/wcd9xxx-mbhc.c
@@ -892,6 +892,7 @@ static void wcd9xxx_report_plug(struct wcd9xxx_mbhc *mbhc, int insertion,
 		hphlocp_off_report(mbhc, SND_JACK_OC_HPHL);
 
 		mbhc->current_plug = PLUG_TYPE_NONE;
+		mbhc->force_linein = false;
 		mbhc->polling_active = false;
 	} else {
 		/*
@@ -920,6 +921,7 @@ static void wcd9xxx_report_plug(struct wcd9xxx_mbhc *mbhc, int insertion,
 						SND_JACK_LINEOUT |
 						SND_JACK_ANC_HEADPHONE |
 						SND_JACK_UNSUPPORTED);
+			mbhc->force_linein = false;
 		}
 
 		/* Report insertion */
@@ -945,6 +947,7 @@ static void wcd9xxx_report_plug(struct wcd9xxx_mbhc *mbhc, int insertion,
 				(mbhc->zr > WCD9XXX_LINEIN_THRESHOLD)) {
 				jack_type = SND_JACK_LINEOUT;
 				mbhc->current_plug = PLUG_TYPE_HIGH_HPH;
+				mbhc->force_linein = true;
 				pr_debug("%s: Replace with SND_JACK_LINEOUT\n",
 				__func__);
 			}
@@ -3182,6 +3185,8 @@ static void wcd9xxx_correct_swch_plug(struct work_struct *work)
 			}
 		} else if (plug_type == PLUG_TYPE_HEADPHONE) {
 			pr_debug("Good headphone detected, continue polling\n");
+			if (mbhc->force_linein)
+				continue;
 			WCD9XXX_BCL_LOCK(mbhc->resmgr);
 			if (mbhc->mbhc_cfg->detect_extn_cable) {
 				if (mbhc->current_plug != plug_type) {
