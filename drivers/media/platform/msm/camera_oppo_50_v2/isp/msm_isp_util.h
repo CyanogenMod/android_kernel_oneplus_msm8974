04999f4aaa2d0695317f1833c1c2a40d619ba580
Sultanxda <sultanxda@gmail.com>
2015-09-07 20:33:16 -0700

    msm: camera: Commonize minimum bus bandwidth rates for the ISP
    Use a single, consistent definition across the ISP drivers.
    
    Change-Id: Ibc73fae74828d88953064bd0b739e04c702c9089

--- drivers/media/platform/msm/camera_oppo_50_v2/isp/msm_isp_util.h
+++ drivers/media/platform/msm/camera_oppo_50_v2/isp/msm_isp_util.h
@@ -22,6 +22,9 @@
 #define ISP_DBG(fmt, args...) pr_debug(fmt, ##args)
 #endif
 
+#define MSM_ISP_MIN_AB 450000000
+#define MSM_ISP_MIN_IB 900000000
+
 #define ALT_VECTOR_IDX(x) {x = 3 - x; }
 struct msm_isp_bandwidth_info {
 	uint32_t active;
