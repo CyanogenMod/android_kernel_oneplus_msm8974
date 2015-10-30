3864dc0c3a7c77d151aac617e1f8e33a75e7e1f1
alucard24 <dmbaoh2@gmail.com>
2015-10-30 10:27:13 +0100

    drivers: state_notifier: use singlethread workqueue
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AudioGod <audiogod@sonic-developers.com>

--- drivers/input/touchscreen/state_notifier.c
+++ drivers/input/touchscreen/state_notifier.c
@@ -154,8 +154,7 @@ static int __init state_notifier_init(void)
 	if (ret)
 		pr_err("Failed to register FB notifier callback for state notifier.\n");
 
-	susp_wq =
-	    alloc_workqueue("state_susp_wq", WQ_HIGHPRI, 0);
+	susp_wq = create_singlethread_workqueue("state_susp_wq");
 	if (!susp_wq)
 		pr_err("State Notifier failed to allocate suspend workqueue\n");
 
