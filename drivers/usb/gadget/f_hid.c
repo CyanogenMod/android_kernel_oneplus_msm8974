c4a5a81ddc841aa03a9c8563ca637c0ecd9c1ab5
binkybear <binkybear@nethunter.com>
2015-11-03 10:23:36 -0600

    Revert "HID Keyboard"
    This reverts commit fde3bae00bade51a34aea42192d798870d7336a1.
    
    # Conflicts:
    #	drivers/usb/gadget/f_hid.c

--- drivers/usb/gadget/f_hid.c
+++ drivers/usb/gadget/f_hid.c
@@ -560,9 +560,14 @@ static int hidg_bind(struct usb_configuration *c, struct usb_function *f)
 
 	hidg_hs_in_ep_desc.bEndpointAddress =
 		hidg_fs_in_ep_desc.bEndpointAddress;
+<<<<<<< HEAD
 
 	/* This does not appear anywhere else and breaks build */
 	/* hidg_hs_out_ep_desc.bEndpointAddress = hidg_fs_out_ep_desc.bEndpointAddress; */
+=======
+	hidg_hs_out_ep_desc.bEndpointAddress =
+		hidg_fs_out_ep_desc.bEndpointAddress;
+>>>>>>> parent of fde3bae... HID Keyboard
 
 	status = usb_assign_descriptors(f, hidg_fs_descriptors,
 			hidg_hs_descriptors, NULL);
