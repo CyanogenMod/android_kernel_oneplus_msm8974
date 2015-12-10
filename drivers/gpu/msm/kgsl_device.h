9a4043f433ec20813cd1ed9ac6a195fee931bfbf
Ajay Dudani <adudani@codeaurora.org>
2015-12-10 12:19:16 -0800

    msm: kgsl: Fix direct references to HZ
    Make the various timeout values HZ agnostic by using the proper
    macros and values instead.
    
    Change-Id: I906b948657c8873518042c7465272c98c5391e59
    Signed-off-by: Suman Tatiraju <sumant@codeaurora.org>
    Signed-off-by: Ajay Dudani <adudani@codeaurora.org>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -30,9 +30,6 @@
 #define KGSL_TIMEOUT_DEFAULT        0xFFFFFFFF
 #define KGSL_TIMEOUT_PART           50 /* 50 msec */
 
-#define FIRST_TIMEOUT (HZ / 2)
-
-
 /* KGSL device state is initialized to INIT when platform_probe		*
  * sucessfully initialized the device.  Once a device has been opened	*
  * (started) it becomes active.  NAP implies that only low latency	*
