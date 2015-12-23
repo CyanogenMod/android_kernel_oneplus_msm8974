58e6b6b9919b95823368e8cfcea651309ba52750
acuicultor <ramoncuenca4@gmail.com>
2015-12-23 08:56:40 +0100

    Keep inline with cm defconfig

--- radioactive-bacon-build.sh
+++ radioactive-bacon-build.sh
@@ -16,7 +16,7 @@ DEFCONFIG="radioactive_defconfig"
 
 # Kernel Details
 BASE_RADIOACTIVE_VER="RADIOACTIVE_REBORN"
-VER="_V2_test"
+VER="_V3_test"
 RADIOACTIVE_VER="$BASE_RADIOACTIVE_VER$VER"
 
 # Vars
