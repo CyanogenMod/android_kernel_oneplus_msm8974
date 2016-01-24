abcc2bb6cf22696f6557a706fe9c30668165021b
acuicultor <ramoncuenca4@gmail.com>
2016-01-24 16:49:55 +0100

    Revert "Hardcode selinux to permissive at kernel level"
    This reverts commit d18f62780c5159c92586ac17dcf0291d18843979.

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -166,8 +166,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	length = -EINVAL;
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
-	
-	new_value = 0; /* inserted this line */
 
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
