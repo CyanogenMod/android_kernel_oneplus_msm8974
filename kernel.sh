b2f17797980c23963b8c789a3c9c8dc77eb95fd0
Jorge Ayres Pereira <jgcaap@gmail.com>
2016-02-16 20:12:29 -0600

    kernel 3.75

--- kernel.sh
+++ kernel.sh
@@ -129,5 +129,5 @@ DATE_END=$(date +"%s")
 DIFF=$(($DATE_END - $DATE_START))
 echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
 echo
-mv ~/new/out/newKernel-CM12-.zip ~/files/oneplusone/kernel/lz4/newKernel-CM13.0-3.74.zip
+mv ~/new/out/newKernel-CM12-.zip ~/files/oneplusone/kernel/newKernel-CM13.0-3.75.zip
 #/etc/script/md5.sh
