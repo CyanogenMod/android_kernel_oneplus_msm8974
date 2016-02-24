7be5d42b1929a973c219f29ae8d0ffb9c44472f7
AdrianDC <radian.dc@gmail.com>
2016-02-24 16:58:43 +0100

    devfreq: Fix mismerge "backport MSM devfreq features from 3.10"
     * Introduced in Iade8855dfef39ce734bf0f3b7e241275d0f7107f
    
    Change-Id: If5a740342d9f5041150e1004fe82fad74e43519d

--- include/linux/of.h
+++ include/linux/of.h
@@ -308,7 +308,6 @@ static inline int of_property_read_u32_index(const struct device_node *np,
 	return -ENOSYS;
 }
 
-<<<<<<< HEAD
 static inline int of_property_read_u8_array(const struct device_node *np,
 			const char *propname, u8 *out_values, size_t sz)
 {
@@ -321,8 +320,6 @@ static inline int of_property_read_u16_array(const struct device_node *np,
 	return -ENOSYS;
 }
 
-=======
->>>>>>> 0ed0fba... devfreq: Backport MSM devfreq features from 3.10
 static inline int of_property_read_u32_array(const struct device_node *np,
 					     const char *propname,
 					     u32 *out_values, size_t sz)
