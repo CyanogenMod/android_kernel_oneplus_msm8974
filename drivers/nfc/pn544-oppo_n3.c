da3b47b1c76a59a4b5d5d2fe1ea8aa67d5e481f5
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-03-21 00:04:41 +0200

    NFC: PN544: Update Oppo variant of NXP PN544 NFC chip driver
    * Original code: find7 5.1.1 LA.BF.1.1.1.c4-02000-8x74.0 sources
    
    Change-Id: I519a3a3392140647cab13d7dd0347661b51a0b9d

--- drivers/nfc/pn544-oppo_n3.c
+++ drivers/nfc/pn544-oppo_n3.c
@@ -28,6 +28,7 @@
 #include <linux/spinlock.h>
 #include <linux/nfc/pn544.h>
 #include <linux/regulator/consumer.h>
+#include <linux/clk.h>
 
 #define MAX_BUFFER_SIZE	512
 
@@ -104,22 +105,11 @@ static ssize_t pn544_dev_read(struct file *filp, char __user * buf,
 
 		pn544_dev->irq_enabled = true;
 		enable_irq(pn544_dev->client->irq);
-
-		ret = enable_irq_wake(pn544_dev->client->irq);
-		if (ret < 0) {
-			pr_err("%s : enable_irq_wake %d\n", __func__, ret);
-		}
-
 		ret = wait_event_interruptible(pn544_dev->read_wq,
 					       gpio_get_value(pn544_dev->
 							      irq_gpio));
 
 		pn544_disable_irq(pn544_dev);
-
-		ret = disable_irq_wake(pn544_dev->client->irq);
-		if (ret < 0) {
-			pr_err("%s : disable_irq_wake %d\n", __func__, ret);
-		}
 
 		if (ret)
 			goto fail;
@@ -196,6 +186,7 @@ static long pn544_dev_ioctl(struct file *filp, unsigned int cmd,
 			    unsigned long arg)
 {
 	struct pn544_dev *pn544_dev = filp->private_data;
+	int ret = 0;
 
 	switch (cmd) {
 	case PN544_SET_PWR:
@@ -203,6 +194,12 @@ static long pn544_dev_ioctl(struct file *filp, unsigned int cmd,
 			/* power on with firmware download (requires hw reset)
 			 */
 			printk("%s power on with firmware\n", __func__);
+			ret = disable_irq_wake(pn544_dev->client->irq);
+			if (ret < 0) {
+				pr_err("%s : power on with firmware "
+				       "disable_irq_wake ret = %d\n",
+				       __func__, ret);
+			}
 			gpio_set_value(pn544_dev->ven_gpio, 1);
 			gpio_set_value(pn544_dev->firm_gpio, 1);
 			msleep(10);
@@ -212,12 +209,24 @@ static long pn544_dev_ioctl(struct file *filp, unsigned int cmd,
 			msleep(10);
 		} else if (arg == 1) {
 			/* power on */
+			ret = enable_irq_wake(pn544_dev->client->irq);
+			if (ret < 0) {
+				pr_err("%s : power on "
+				       "enable_irq_wake ret = %d\n",
+				       __func__, ret);
+			}
 			printk("%s power on\n", __func__);
 			gpio_set_value(pn544_dev->firm_gpio, 0);
 			gpio_set_value(pn544_dev->ven_gpio, 1);
 			msleep(10);
 		} else if (arg == 0) {
 			/* power off */
+			ret = disable_irq_wake(pn544_dev->client->irq);
+			if (ret < 0) {
+				printk("%s : power off "
+				       "disable_irq_wake ret = %d\n",
+				       __func__, ret);
+			}
 			printk("%s power off\n", __func__);
 			gpio_set_value(pn544_dev->firm_gpio, 0);
 			gpio_set_value(pn544_dev->ven_gpio, 0);
@@ -249,7 +258,8 @@ static int pn544_probe(struct i2c_client *client,
 {
 	int ret;
 	struct pn544_i2c_platform_data *platform_data;
-	struct pn544_dev *pn544_dev;
+	struct pn544_dev *pn544_dev = NULL;
+	struct clk *p65T_clk = NULL;
 
 	client->dev.platform_data = &nfc_pdata;
 
@@ -280,6 +290,19 @@ static int pn544_probe(struct i2c_client *client,
 	if (ret) {
 		pr_err("gpio_nfc_firm request error\n");
 		return -ENODEV;
+	}
+
+	p65T_clk = clk_get(&client->dev, "cxo_out_d1");
+	if (p65T_clk == NULL) {
+		pr_err("pn65T clk error");
+		goto err_exit;
+	}
+
+	ret = clk_prepare_enable(p65T_clk);
+	if (ret) {
+		pr_err("pn65T clk_prepare_disable ret = %d", ret);
+		p65T_clk = NULL;
+		clk_disable_unprepare(p65T_clk);
 	}
 
 	pn544_dev = kzalloc(sizeof(*pn544_dev), GFP_KERNEL);
@@ -344,6 +367,10 @@ static int pn544_probe(struct i2c_client *client,
 
 	pn544_disable_irq(pn544_dev);
 	i2c_set_clientdata(client, pn544_dev);
+
+	gpio_set_value(pn544_dev->ven_gpio, 1);
+	msleep(10);
+	gpio_set_value(pn544_dev->ven_gpio, 0);
 
 	return 0;
 
