9672c02fe72a14e1c1843349f3677283810b7632
Chao Yu <yuchao0@huawei.com>
2016-04-29 20:09:15 +0800

    f2fs: fix to clear private data in page
    Private data in page should be removed during ->releasepage or
    ->invalidatepage, otherwise garbage data would be remained in that page.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1749,6 +1749,7 @@ void f2fs_invalidate_page(struct page *page, unsigned long offset)
 	if (IS_ATOMIC_WRITTEN_PAGE(page))
 		return;
 
+	set_page_private(page, 0);
 	ClearPagePrivate(page);
 }
 
@@ -1762,6 +1763,7 @@ int f2fs_release_page(struct page *page, gfp_t wait)
 	if (IS_ATOMIC_WRITTEN_PAGE(page))
 		return 0;
 
+	set_page_private(page, 0);
 	ClearPagePrivate(page);
 	return 1;
 }
