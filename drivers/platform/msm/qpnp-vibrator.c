51fcbc5c718cf18dca56547000793168061e80e1
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-10 22:37:35 +0200

    drivers/msm: qpnp-vibrator: Remove workqueue leftover
    * Leftover of commit 1de664f7049e0534ade35760df00fac576bf5d3c
      which switched qpnp-vibrator from workqueue to spinlocks
    
    Change-Id: Ibd24bb72787e15aea3e70e4c791c059fd0bb4852

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -39,7 +39,6 @@ struct qpnp_vib {
 	struct spmi_device *spmi;
 	struct hrtimer vib_timer;
 	struct timed_output_dev timed_dev;
-	struct work_struct work;
 
 	u8  reg_vtg_ctl;
 	u8  reg_en_ctl;
@@ -263,13 +262,6 @@ retry:
 	spin_unlock_irqrestore(&vib->lock, flags);
 }
 
-static void qpnp_vib_update(struct work_struct *work)
-{
-	struct qpnp_vib *vib = container_of(work, struct qpnp_vib,
-					 work);
-	qpnp_vib_set(vib, vib->state);
-}
-
 static int qpnp_vib_get_time(struct timed_output_dev *dev)
 {
 	struct qpnp_vib *vib = container_of(dev, struct qpnp_vib,
@@ -304,7 +296,6 @@ static int qpnp_vibrator_suspend(struct device *dev)
 	struct qpnp_vib *vib = dev_get_drvdata(dev);
 
 	hrtimer_cancel(&vib->vib_timer);
-	cancel_work_sync(&vib->work);
 	/* turn-off vibrator */
 	qpnp_vib_set(vib, 0);
 
@@ -392,7 +383,6 @@ static int __devinit qpnp_vibrator_probe(struct spmi_device *spmi)
 	vib->reg_en_ctl = val;
 
 	spin_lock_init(&vib->lock);
-	INIT_WORK(&vib->work, qpnp_vib_update);
 
 	hrtimer_init(&vib->vib_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
 	vib->vib_timer.function = qpnp_vib_timer_func;
@@ -439,7 +429,6 @@ static int  __devexit qpnp_vibrator_remove(struct spmi_device *spmi)
 {
 	struct qpnp_vib *vib = dev_get_drvdata(&spmi->dev);
 
-	cancel_work_sync(&vib->work);
 	hrtimer_cancel(&vib->vib_timer);
 	device_remove_file(vib->timed_dev.dev, &dev_attr_vtg_level);
 	device_remove_file(vib->timed_dev.dev, &dev_attr_vtg_min);
