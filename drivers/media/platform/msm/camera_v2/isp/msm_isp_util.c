91c9804b2941f5ad77823207da77493da08e4dd4
Vijaya Kumar T M <vtmuni@codeaurora.org>
2016-07-11 13:08:32 +0530

    msm: camera: isp: Fix warning and errors based on static analysis
    This change fixes the warning/errors from static analysis
    
    Conflicts:
            drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
            drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
            drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
    
    CRs-fixed: 992942
    Change-Id: Iaf90ab4c1d17f903d03458d76cab1b4c0a5c8836
    Signed-off-by: Jing Zhou <jzhou70@codeaurora.org>
    Signed-off-by: Vijaya Kumar T M <vtmuni@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1020,8 +1020,6 @@ void msm_isp_update_error_frame_count(struct vfe_device *vfe_dev)
 {
 	struct msm_vfe_error_info *error_info = &vfe_dev->error_info;
 	error_info->info_dump_frame_count++;
-	if (error_info->info_dump_frame_count == 0)
-		error_info->info_dump_frame_count++;
 }
 
 void msm_isp_process_error_info(struct vfe_device *vfe_dev)
