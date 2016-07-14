abf82091261712548a8d48791028e5337f7e6ef3
Jaegeuk Kim <jaegeuk@kernel.org>
2016-07-13 19:33:19 -0700

    f2fs: use blk_plug in all the possible paths
    This patch reverts 19a5f5e2ef37 (f2fs: drop any block plugging),
    and adds blk_plug in write paths additionally.
    
    The main reason is that blk_start_plug can be used to wake up from low-power
    mode before submitting further bios.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
     Conflicts:
    	fs/f2fs/file.c

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -2131,6 +2131,7 @@ static ssize_t f2fs_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = file_inode(file);
 	size_t count;
+	struct blk_plug plug;
 	ssize_t ret;
 
 	if (f2fs_encrypted_inode(inode) &&
@@ -2147,9 +2148,12 @@ static ssize_t f2fs_file_aio_write(struct kiocb *iocb, const struct iovec *iov,
 	if (!ret) {
 		ret = f2fs_preallocate_blocks(inode, pos, count,
 				iocb->ki_filp->f_flags & O_DIRECT);
-		if (!ret)
+		if (!ret) {
+			blk_start_plug(&plug);
 			ret = __generic_file_aio_write(iocb, iov, nr_segs,
 								&iocb->ki_pos);
+			blk_finish_plug(&plug);
+		}
 	}
 	inode_unlock(inode);
 
