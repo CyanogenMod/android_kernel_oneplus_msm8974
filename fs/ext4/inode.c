b090bf3f7c988dfd862a8d4242aa86d7467d3ba7
Nick Desaulniers <ndesaulniers@google.com>
2016-07-18 12:45:17 -0700

    fs: ext4: disable support for fallocate FALLOC_FL_PUNCH_HOLE
    Bug: 28760453
    Change-Id: I019c2de559db9e4b95860ab852211b456d78c4ca
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3392,6 +3392,7 @@ int ext4_can_truncate(struct inode *inode)
 
 int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 {
+#if 0
 	struct inode *inode = file->f_path.dentry->d_inode;
 	if (!S_ISREG(inode->i_mode))
 		return -EOPNOTSUPP;
@@ -3407,6 +3408,12 @@ int ext4_punch_hole(struct file *file, loff_t offset, loff_t length)
 	}
 
 	return ext4_ext_punch_hole(file, offset, length);
+#else
+	/*
+	 * Disabled as per b/28760453
+	 */
+	return -EOPNOTSUPP;
+#endif
 }
 
 /*
