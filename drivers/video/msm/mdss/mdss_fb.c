234402b166a6d0a6306939d6a5a9c0632fcd5d2d
Danny Baumann <dannybaumann@web.de>
2016-08-17 11:57:24 +0200

    mdss: Restore last backlight value on panel on.
    Change-Id: I488f515d3cde64d31eaeac517b9cbdf0ae9b6851

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1105,11 +1105,16 @@ static int mdss_fb_blank_blank(struct msm_fb_data_type *mfd,
 
 	mfd->op_enable = false;
 	if (mdss_panel_is_power_off(req_power_state)) {
+		/* save bl level to restore on unblank */
+		u32 bl_level = mfd->unset_bl_level
+				? mfd->unset_bl_level : mfd->bl_level;
+
 		/* Stop Display thread */
 		if (mfd->disp_thread)
 			mdss_fb_stop_disp_thread(mfd);
 		mutex_lock(&mfd->bl_lock);
 		mdss_fb_set_backlight(mfd, 0);
+		mfd->unset_bl_level = bl_level;
 		mfd->bl_updated = 0;
 		mutex_unlock(&mfd->bl_lock);
 	}
