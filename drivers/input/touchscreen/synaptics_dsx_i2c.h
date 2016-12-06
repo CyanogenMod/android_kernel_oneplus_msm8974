9f0e18cac745809e90084617ae7b05f23f94bd0f
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-12-06 21:03:54 +0200

    input: synaptics: Support up arrow gesture
    * Cleanup the code a bit while we are at it
    
    Change-Id: Idb14c27594a914c5edf587a45a244f3a6290c34e

--- drivers/input/touchscreen/synaptics_dsx_i2c.h
+++ drivers/input/touchscreen/synaptics_dsx_i2c.h
@@ -72,10 +72,10 @@
 #define MASK_1BIT 0x01
 
 
-#define TP_VENDOR_WINTEK	1	//Ê¤»ª
+#define TP_VENDOR_WINTEK	1	//Ê¤ï¿½ï¿½
 #define TP_VENDOR_TPK		2	//TPK
-#define TP_VENDOR_TRULY		3	//ÐÅÀû
-#define TP_VENDOR_YOUNGFAST 4   //Ñó»ª
+#define TP_VENDOR_TRULY		3	//ï¿½ï¿½ï¿½ï¿½
+#define TP_VENDOR_YOUNGFAST 4   //ï¿½ï¿½ï¿½ï¿½
 
 #define TP_TYPE_MAX		2	//we only use wintek and tpk now.
 
@@ -282,6 +282,7 @@ struct synaptics_rmi4_data {
 	atomic_t syna_use_gesture;
 	atomic_t double_tap_enable;
 	atomic_t double_swipe_enable;
+	atomic_t up_arrow_enable;
 	atomic_t down_arrow_enable;
 	atomic_t left_arrow_enable;
 	atomic_t right_arrow_enable;
--- include/linux/input.h
+++ include/linux/input.h
@@ -478,12 +478,12 @@ struct input_keymap_entry {
 #define KEY_MICMUTE		248	/* Mute / unmute the microphone */
 
 /* Oppo specific keycodes */
-#define KEY_GESTURE_CIRCLE	250
-#define KEY_GESTURE_SWIPE_DOWN	251
-#define KEY_GESTURE_V		252
-#define KEY_GESTURE_LTR		253
-#define KEY_GESTURE_GTR		254
-#define KEY_DOUBLE_TAP      255
+#define KEY_GESTURE_CIRCLE		250
+#define KEY_GESTURE_DOUBLE_SWIPE	251
+#define KEY_GESTURE_DOWN_ARROW		252
+#define KEY_GESTURE_LEFT_ARROW		253
+#define KEY_GESTURE_RIGHT_ARROW		254
+#define KEY_GESTURE_UP_ARROW		255
 
 /* Code 255 is reserved for special needs of AT keyboard driver */
 
