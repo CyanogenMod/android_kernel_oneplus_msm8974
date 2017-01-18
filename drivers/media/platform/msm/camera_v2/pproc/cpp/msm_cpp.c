f9368a9de96c4734f4caeb1ffc1734c05de39902
Ariel Yin <ayin@google.com>
2017-01-17 17:04:09 -0800

    msm: cpp: Fix for integer overflow in cpp
    Due to integer overflow, the bound check in config frame function
    may pass and this may allow user to access invalid buffer. This
    fix takes care of proper bound and don't allow integer overflow.
    
    Bug: 32919951
    CRs-Fixed: 1097709
    Change-Id: I504ad591633afaba82268b5ee27a321691d75c80
    Signed-off-by: Kumar Behera <mohanb@codeaurora.org>
    Signed-off-by: Biswajit Paul <biswajitpaul@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
+++ drivers/media/platform/msm/camera_v2/pproc/cpp/msm_cpp.c
@@ -1306,7 +1306,7 @@ static int msm_cpp_cfg(struct cpp_device *cpp_dev,
 	struct msm_buf_mngr_info buff_mgr_info, dup_buff_mgr_info;
 	int32_t status = 0;
 	int in_fd;
-	int32_t stripe_base = 0;
+	uint32_t stripe_base = 0;
 	int i = 0;
 	if (!new_frame) {
 		pr_err("Insufficient memory. return\n");
@@ -1357,6 +1357,14 @@ static int msm_cpp_cfg(struct cpp_device *cpp_dev,
 		pr_err("%s %d Invalid frame message\n", __func__, __LINE__);
 		return -EINVAL;
 	}
+
+	if (stripe_base == UINT_MAX || new_frame->num_strips >
+		(UINT_MAX - 1 - stripe_base) / 27) {
+		pr_err("Invalid frame message,num_strips %d is large\n",
+			new_frame->num_strips);
+		return -EINVAL;
+	}
+
 	in_phyaddr = msm_cpp_fetch_buffer_info(cpp_dev,
 		&new_frame->input_buffer_info,
 		((new_frame->input_buffer_info.identity >> 16) & 0xFFFF),
