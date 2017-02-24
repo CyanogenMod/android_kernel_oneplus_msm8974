85d9c044660cbec5fa6c63939694b49a40499c51
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2017-02-23 18:24:42 -0700

    net: rmnet_data: Add support to configure custom device name
    rmnet_data assigns device name by the order they are created.
    This causes problems which multiple processes are trying to
    create devices and leads to random device names.
    
    Assign device name as specified by user.
    
    CRs-Fixed: 2018785
    Change-Id: Iab8e053c6ccacbeedaa7763e760d0c12e756b5d0
    Signed-off-by: Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
    [haggertk: Backport to 3.4. Header update only, necessary to compile
     librmnetctl. This header, in particular the enum being modified, are
     not actually used by our kernel at all, just exported for userspace
     builds.
     See for full original:
     https://source.codeaurora.org/quic/la/kernel/msm-4.4/commit/?id=b268a55a9f7a7 ]
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- include/linux/rmnet_data.h
+++ include/linux/rmnet_data.h
@@ -208,8 +208,19 @@ enum rmnet_netlink_message_types_e {
 	 *       uint32_t MAP Flow Handle
 	 * Returns: status code
 	 */
-	RMNET_NETLINK_DEL_VND_TC_FLOW
+	RMNET_NETLINK_DEL_VND_TC_FLOW,
+
+	/*
+	 * RMNET_NETLINK_NEW_VND_WITH_NAME - Creates a new virtual network
+	 *                                   device node with the specified
+	 *                                   device name
+	 * Args: int32_t node number
+	 *       char[] vnd_name - Use as name
+	 * Returns: status code
+	 */
+	RMNET_NETLINK_NEW_VND_WITH_NAME
 };
+#define RMNET_NETLINK_NEW_VND_WITH_NAME RMNET_NETLINK_NEW_VND_WITH_NAME
 
 enum rmnet_config_endpoint_modes_e {
 	/* Pass the frame up the stack with no modifications to skb->dev      */
