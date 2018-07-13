9ba5cc9777cc331d9e26e65d523f38b134eeda2d
Kevin F. Haggerty <haggertk@lineageos.org>
2018-07-13 06:36:42 -0600

    msm: ipa: Add declaration of ipa_del_hdr_by_user
    An incomplete backport/patch failed to add this declaration to
    ipa_i.h
    
    Change-Id: I34d07ec7362c2aff8e652fec9bf30287aa51efb5
    Fixes: c5b59371f93d ("msm: ipa: Prevent multiple header deletion from user space")
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/platform/msm/ipa/ipa_i.h
+++ drivers/platform/msm/ipa/ipa_i.h
@@ -825,6 +825,7 @@ void ipa_enable_clks(void);
 void ipa_disable_clks(void);
 void ipa_inc_client_enable_clks(void);
 void ipa_dec_client_disable_clks(void);
+int ipa_del_hdr_by_user(struct ipa_ioc_del_hdr *hdls, bool by_user);
 int __ipa_del_rt_rule(u32 rule_hdl);
 int __ipa_del_hdr(u32 hdr_hdl);
 int __ipa_release_hdr(u32 hdr_hdl);
