449106c38d6a449c10fc38a3c1e35f4cdbed20ae
Will Deacon <will@kernel.org>
2019-10-04 10:51:32 +0100

    cfg80211: wext: Reject malformed SSID elements
    Ensure the SSID element is bounds-checked prior to invoking memcpy()
    with its length field.
    
    Cc: <stable@vger.kernel.org>
    Cc: Johannes Berg <johannes@sipsolutions.net>
    Cc: Kees Cook <keescook@chromium.org>
    Reported-by: Nicolas Waisman <nico@semmle.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    CVE-2019-17133
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Change-Id: If6a80f42391e0fef49c66260e4ef47000197e095

--- net/wireless/wext-sme.c
+++ net/wireless/wext-sme.c
@@ -217,6 +217,7 @@ int cfg80211_mgd_wext_giwessid(struct net_device *dev,
 			       struct iw_request_info *info,
 			       struct iw_point *data, char *ssid)
 {
+	int ret = 0;
 	struct wireless_dev *wdev = dev->ieee80211_ptr;
 
 	/* call only for station! */
@@ -232,7 +233,10 @@ int cfg80211_mgd_wext_giwessid(struct net_device *dev,
 		if (ie) {
 			data->flags = 1;
 			data->length = ie[1];
-			memcpy(ssid, ie + 2, data->length);
+			if (data->length > IW_ESSID_MAX_SIZE)
+				ret = -EINVAL;
+			else
+				memcpy(ssid, ie + 2, data->length);
 		}
 	} else if (wdev->wext.connect.ssid && wdev->wext.connect.ssid_len) {
 		data->flags = 1;
@@ -241,7 +245,7 @@ int cfg80211_mgd_wext_giwessid(struct net_device *dev,
 	}
 	wdev_unlock(wdev);
 
-	return 0;
+	return ret;
 }
 
 int cfg80211_mgd_wext_siwap(struct net_device *dev,
