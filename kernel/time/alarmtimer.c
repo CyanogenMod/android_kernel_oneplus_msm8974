e9003fdadddb44f17c31c2090dd25bac846d40c5
Petr Vorel <pvorel@suse.cz>
2019-11-08 16:51:16 +0100

    alarmtimer: Change remaining ENOTSUPP to EOPNOTSUPP
    Fix backport of commit f18ddc13af981ce3c7b7f26925f099e7c6929aba upstream.
    
    Update backport to change ENOTSUPP to EOPNOTSUPP in
    alarm_timer_{del,set}(), which were removed in
    f2c45807d3992fe0f173f34af9c347d907c31686 in v4.13-rc1.
    
    Fixes: c22df8ea7c5831d6fdca2f6f136f0d32d7064ff9
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: I21d504d246f8082f819ba7d28c2ecec74cf9841f

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -772,7 +772,7 @@ static void alarm_timer_get(struct k_itimer *timr,
 static int alarm_timer_del(struct k_itimer *timr)
 {
 	if (!rtcdev)
-		return -ENOTSUPP;
+		return -EOPNOTSUPP;
 
 	if (alarm_try_to_cancel(&timr->it.alarm.alarmtimer) < 0)
 		return TIMER_RETRY;
@@ -796,7 +796,7 @@ static int alarm_timer_set(struct k_itimer *timr, int flags,
 	ktime_t exp;
 
 	if (!rtcdev)
-		return -ENOTSUPP;
+		return -EOPNOTSUPP;
 
 	if (flags & ~TIMER_ABSTIME)
 		return -EINVAL;
