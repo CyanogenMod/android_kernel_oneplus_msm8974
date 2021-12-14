7e588387c2223c2dae9f938612c390b26c31b6c2
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2021-12-14 19:46:21 +0100

    FROMGIT: USB: gadget: bRequestType is a bitfield, not a enum
    Szymon rightly pointed out that the previous check for the endpoint
    direction in bRequestType was not looking at only the bit involved, but
    rather the whole value.  Normally this is ok, but for some request
    types, bits other than bit 8 could be set and the check for the endpoint
    length could not stall correctly.
    
    Fix that up by only checking the single bit.
    
    Fixes: 153a2d7e3350 ("USB: gadget: detect too-big endpoint 0 requests")
    Cc: Felipe Balbi <balbi@kernel.org>
    Reported-by: Szymon Heidrich <szymon.heidrich@gmail.com>
    Link: https://lore.kernel.org/r/20211214184621.385828-1-gregkh@linuxfoundation.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit f08adf5add9a071160c68bb2a61d697f39ab0758
     https://git.kernel.org/pub/scm/linux/kernel/git/gregkh/usb.git usb-linus)
    Bug: 210292376
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I7e708b2b94433009c87f697346e0515d93454f48
    CVE-2021-39685
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1117,14 +1117,14 @@ composite_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *ctrl)
 
 
 	if (w_length > USB_BUFSIZ) {
-		if (ctrl->bRequestType == USB_DIR_OUT) {
-			goto done;
-		} else {
+		if (ctrl->bRequestType & USB_DIR_IN) {
 			/* Cast away the const, we are going to overwrite on purpose. */
 			__le16 *temp = (__le16 *)&ctrl->wLength;
 
 			*temp = cpu_to_le16(USB_BUFSIZ);
 			w_length = USB_BUFSIZ;
+		} else {
+			goto done;
 		}
 	}
 
