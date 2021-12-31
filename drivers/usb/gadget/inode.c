1e5e806cc8873d16e9a10bb4fa7423aad59fec19
Hangyu Hua <hbh25y@gmail.com>
2022-01-01 01:21:37 +0800

    UPSTREAM: usb: gadget: don't release an existing dev->buf
    commit 89f3594d0de58e8a57d92d497dea9fee3d4b9cda upstream.
    
    dev->buf does not need to be released if it already exists before
    executing dev_config.
    
    Bug: 220261709
    Acked-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Hangyu Hua <hbh25y@gmail.com>
    Link: https://lore.kernel.org/r/20211231172138.7993-2-hbh25y@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Id53d6770fbae0a7fcf0fa136157c0ab34fb5da64
    CVE-2022-24958
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/usb/gadget/inode.c
+++ drivers/usb/gadget/inode.c
@@ -1877,8 +1877,10 @@ dev_config (struct file *fd, const char __user *buf, size_t len, loff_t *ptr)
 
 	spin_lock_irq (&dev->lock);
 	value = -EINVAL;
-	if (dev->buf)
-		goto fail;
+	if (dev->buf) {
+		spin_unlock_irq(&dev->lock);
+		return value;
+	}
 	dev->buf = kbuf;
 
 	/* full or low speed config */
