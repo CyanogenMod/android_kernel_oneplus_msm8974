85c01660f793b7d0a9ce6d26da61ef67544553ce
Szymon Heidrich <szymon.heidrich@gmail.com>
2022-01-24 12:14:00 +0100

    UPSTREAM: USB: gadget: validate interface OS descriptor requests
    Stall the control endpoint in case provided index exceeds array size of
    MAX_CONFIG_INTERFACES or when the retrieved function pointer is null.
    
    Bug: 213172319
    Signed-off-by: Szymon Heidrich <szymon.heidrich@gmail.com>
    Cc: stable@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit 75e5b4849b81e19e9efe1654b30d7f3151c33c2c)
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I78f46b6f2140394a6bc6cff9f829c0742d7ad2fc
    (cherry picked from commit c7732dbce590ef33ac2345f21efa6703f78b9e95)
    CVE-2022-25258
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1358,7 +1358,8 @@ unknown:
 		 */
 		switch (ctrl->bRequestType & USB_RECIP_MASK) {
 		case USB_RECIP_INTERFACE:
-			if (!cdev->config || intf >= MAX_CONFIG_INTERFACES)
+			if (!cdev->config || intf >= MAX_CONFIG_INTERFACES ||
+			    !cdev->config->interface[intf])
 				break;
 			f = cdev->config->interface[intf];
 			break;
